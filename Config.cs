6605bce69b8198358c18cd620007f51f35c157b4
sindrekjr <23259585+sindrekjr@users.noreply.github.com>
2019-12-01 00:44:07 +0100

    Initial commit

---
+++ Config.cs
@@ -0,0 +1,75 @@
+using System; 
+using System.IO; 
+using System.Linq; 
+using System.Text.Json; 
+using System.Text.RegularExpressions; 
+
+namespace AdventOfCode {
+
+    class Config {
+
+        string _c; 
+        int _y;
+        int[] _d;
+
+        public string Cookie { 
+            get => _c;
+            set {
+                if(Regex.IsMatch(value, "^session=[a-z0-9]+$")) {
+                    _c = value; 
+                } else {
+                    _c = "";
+                }
+            }
+        }
+        public int Year { 
+            get => _y;
+            set {
+                if(value < 2015 || value > DateTime.Now.Year) {
+                    _y = DateTime.Now.Year; 
+                } else {
+                    _y = value; 
+                }
+            } 
+        }
+        public int[] Days { 
+            get => _d;
+            set {
+                bool allDaysCovered = false;
+                _d = value.Where(v => {
+                    if(v == 0) allDaysCovered = true; 
+                    return v > 0 && v < 26;
+                }).ToArray();
+
+                if(allDaysCovered) {
+                    _d = new int[]{0};
+                } else {
+                    Array.Sort(_d);
+                }
+            }
+        }
+
+        public static Config Get(string path) {
+            var options = new JsonSerializerOptions(){
+                IgnoreNullValues = true,
+                PropertyNameCaseInsensitive = true,
+                WriteIndented = true
+            };
+            Config config; 
+            if(File.Exists(path)) {
+                config = JsonSerializer.Deserialize<Config>(File.ReadAllText(path), options);
+            } else {
+                config = new Config();
+                config.setDefaults(); 
+                File.WriteAllText(path, JsonSerializer.Serialize<Config>(config, options));
+            }
+            return config; 
+        }
+
+        void setDefaults() {
+            Cookie = ""; 
+            Year = DateTime.Now.Year;
+            Days = (DateTime.Now.Month == 12) ? new int[]{DateTime.Now.Day} : new int[]{0}; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode.Solutions;
+using System;
+
+namespace AdventOfCode {
+
+    class Program {
+
+        public static Config Config = Config.Get("config.json"); 
+        static SolutionCollector Solutions = new SolutionCollector(Config.Year, Config.Days); 
+
+        static void Main(string[] args) {
+            foreach(ASolution solution in Solutions) {
+                solution.Solve();
+            }
+        }
+    }
+}
