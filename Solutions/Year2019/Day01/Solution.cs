0614f2291044a4a8c2f6bbabe7b6fd5fc8d9f8c6
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-01 10:06:16 +0100

    Day 01

--- Solutions/Year2019/Day01/Solution.cs
+++ Solutions/Year2019/Day01/Solution.cs
@@ -1,17 +1,33 @@
+using System; 
+using System.Linq; 
+
 namespace AdventOfCode.Solutions.Year2019 {
 
     class Day01 : ASolution {
         
-        public Day01() : base(1, 2019, "") {
+        public Day01() : base(1, 2019, "The Tyranny of the Rocket Equation") {
             
         }
 
+
+
         protected override string SolvePartOne() {
-            return null; 
+            return Input.SplitByNewline()
+                .Select(mass => int.Parse(mass) / 3 - 2)
+                .ToArray<int>()
+                .Sum().ToString();
         }
 
         protected override string SolvePartTwo() {
-            return null; 
+            return Input.SplitByNewline()
+                .Select(mass => FuelFuel(int.Parse(mass)))
+                .ToArray<int>()
+                .Sum().ToString();
+        }
+
+        int FuelFuel(int module) {
+            int fuel = module / 3 - 2; 
+            return fuel <= 0 ? 0 : fuel + FuelFuel(fuel); 
         }
     }
 }
