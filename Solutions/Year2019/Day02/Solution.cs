a1579eb97a9ab016cc80b310f3b9ca91195005f6
Sindre S. Kjær <23259585+sindrekjr@users.noreply.github.com>
2019-12-02 14:04:37 +0100

    Cleanup Day 2

--- Solutions/Year2019/Day02/Solution.cs
+++ Solutions/Year2019/Day02/Solution.cs
@@ -10,9 +10,23 @@ namespace AdventOfCode.Solutions.Year2019 {
         }
 
         protected override string SolvePartOne() {
-            int[] code = Input.ToIntArray(",");
-            code[1] = 12; 
-            code[2] = 2; 
+            return Input.ToIntArray(",").Run(12, 2)[0].ToString();
+        }
+
+        protected override string SolvePartTwo() {
+            foreach(int noun in Enumerable.Range(0, 99)) {
+                foreach(int verb in Enumerable.Range(0, 99)) {
+                    if(Input.ToIntArray(",").Run(noun, verb)[0] == 19690720) {
+                        return (100 * noun + verb).ToString(); 
+                    }
+                }
+            }
+            return null; 
+        }
+        
+        int[] Run(this int[] code, int noun, int verb) {
+            code[1] = noun;
+            code[2] = verb; 
             for(int i = 0; i < code.Length; i++) {
                 int output;
                 if(code[i] == 1) {
@@ -26,32 +40,7 @@ namespace AdventOfCode.Solutions.Year2019 {
                 }
                 code[code[++i]] = output;
             }
-            return code[0].ToString(); 
-        }
-
-        protected override string SolvePartTwo() {
-            foreach(int noun in Enumerable.Range(0, 99)) {
-                foreach(int verb in Enumerable.Range(0, 99)) {
-                    int[] code = Input.ToIntArray(",");
-                    code[1] = noun;
-                    code[2] = verb; 
-                    for(int i = 0; i < code.Length; i++) {
-                        int output;
-                        if(code[i] == 1) {
-                            output = code[code[++i]] + code[code[++i]];
-                        } else if(code[i] == 2) {
-                            output = code[code[++i]] * code[code[++i]];
-                        } else if(code[i] == 99) {
-                            break; 
-                        } else {
-                            throw new SomethingWentWrongException(); 
-                        }
-                        code[code[++i]] = output;
-                    }
-                    if(code[0] == 19690720) return (100 * noun + verb).ToString(); 
-                }
-            }
-            return null; 
+            return code; 
         }
 
         class SomethingWentWrongException : Exception {
