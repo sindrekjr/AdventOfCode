63757f56f73bb2989ed23d83bd80b6fb159cbe50
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-02 08:41:31 +0100

    Solved day 2, part 1

--- Solutions/Year2019/Day02/Solution.cs
+++ Solutions/Year2019/Day02/Solution.cs
@@ -1,17 +1,39 @@
+using System;
+
 namespace AdventOfCode.Solutions.Year2019 {
 
     class Day02 : ASolution {
         
-        public Day02() : base(2, 2019, "") {
+        public Day02() : base(2, 2019, "1202 Program Alarm") {
             
         }
 
         protected override string SolvePartOne() {
-            return null; 
+            int[] code = Input.ToIntArray(",");
+            code[1] = 12; 
+            code[2] = 2; 
+            for(int i = 0; i < code.Length; i++) {
+                int output;
+                if(code[i] == 1) {
+                    output = code[code[++i]] + code[code[++i]];
+                } else if(code[i] == 2) {
+                    output = code[code[++i]] * code[code[++i]];
+                } else if(code[i] == 99) {
+                    break; 
+                } else {
+                    throw new SomethingWentWrongException(); 
+                }
+                code[code[++i]] = output; 
+            }
+            return code[0].ToString(); 
         }
 
         protected override string SolvePartTwo() {
             return null; 
+        }
+
+        class SomethingWentWrongException : Exception {
+
         }
     }
 }
