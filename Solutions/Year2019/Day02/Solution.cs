32dde2c3aede67a00b0916243c280337548c3adc
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-02 09:03:12 +0100

    Solved day 2, part 2

--- Solutions/Year2019/Day02/Solution.cs
+++ Solutions/Year2019/Day02/Solution.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq; 
 
 namespace AdventOfCode.Solutions.Year2019 {
 
@@ -23,12 +24,33 @@ namespace AdventOfCode.Solutions.Year2019 {
                 } else {
                     throw new SomethingWentWrongException(); 
                 }
-                code[code[++i]] = output; 
+                code[code[++i]] = output;
             }
             return code[0].ToString(); 
         }
 
         protected override string SolvePartTwo() {
+            foreach(int noun in Enumerable.Range(0, 99)) {
+                foreach(int verb in Enumerable.Range(0, 99)) {
+                    int[] code = Input.ToIntArray(",");
+                    code[1] = noun;
+                    code[2] = verb; 
+                    for(int i = 0; i < code.Length; i++) {
+                        int output;
+                        if(code[i] == 1) {
+                            output = code[code[++i]] + code[code[++i]];
+                        } else if(code[i] == 2) {
+                            output = code[code[++i]] * code[code[++i]];
+                        } else if(code[i] == 99) {
+                            break; 
+                        } else {
+                            throw new SomethingWentWrongException(); 
+                        }
+                        code[code[++i]] = output;
+                    }
+                    if(code[0] == 19690720) return (100 * noun + verb).ToString(); 
+                }
+            }
             return null; 
         }
 
