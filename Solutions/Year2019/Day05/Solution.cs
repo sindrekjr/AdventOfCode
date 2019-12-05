af2432f7b52b74abcdd41f9d7d8a8b7ebc8a16a1
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-05 18:11:57 +0100

    Solved Day 5 Part 1

--- Solutions/Year2019/Day05/Solution.cs
+++ Solutions/Year2019/Day05/Solution.cs
@@ -2,12 +2,16 @@ namespace AdventOfCode.Solutions.Year2019 {
 
     class Day05 : ASolution {
 
-        public Day05() : base(5, 2019, "") {
+        IntcodeComputer Machine;
 
-        }
+        public Day05() : base(5, 2019, "Sunny with a Chance of Asteroids") {
+            string DebugInput = "3,0,4,0,99"; 
+            Machine = new IntcodeComputer(Input.ToIntArray(","));
+        }        
 
         protected override string SolvePartOne() {
-            return null;
+            Machine.Run(1).ToString(); 
+            return Machine.Diagnose().ToString(); 
         }
 
         protected override string SolvePartTwo() {
--- Solutions/Year2019/IntcodeComputer.cs
+++ Solutions/Year2019/IntcodeComputer.cs
@@ -1,41 +1,73 @@
 using System; 
+using System.Collections.Generic; 
+using System.Linq; 
 
 namespace AdventOfCode.Solutions.Year2019 {
     class IntcodeComputer {
 
+        bool initialized = false; 
         readonly int[] intcode; 
+        int[] memory; 
+
+        public List<int> Output;
 
         public IntcodeComputer(int[] intcode) {
-            this.intcode = intcode; 
+            this.intcode = intcode;
         }
 
-        public int[] Run(Nullable<int> noun = null, Nullable<int> verb = null) {
-            int[] code = new int[intcode.Length]; 
-            intcode.CopyTo(code, 0);
-            if(noun != null) code[1] = noun.Value; 
-            if(verb != null) code[2] = verb.Value; 
-            for(int i = 0; i < code.Length; i++) {
-                int opcode = GetOpcode(code[i]); 
+        public void Initialize(int? noun = null, int? verb = null) {
+            memory = new int[intcode.Length];
+            intcode.CopyTo(memory, 0);
+            if(noun != null) memory[1] = noun.Value; 
+            if(verb != null) memory[2] = verb.Value; 
+
+            Output = new List<int>(); 
+            initialized = true; 
+        } 
+
+        public int[] Run(int? input = null) {
+            if(!initialized) Initialize(); 
+            for(int i = 0; i < memory.Length; i++) {
+                (string modes, Opcode opcode) = ParseInstruction(memory[i]); 
+                //Console.WriteLine($"[{i}]  Opcode: {opcode}, Modes: {modes[0]}, {modes[1]}, {modes[2]}");
+                string debug = $"[{i}] {memory[i]}  :: {opcode}"; 
                 int output;
-                if(opcode == 1) {
-                    output = code[code[++i]] + code[code[++i]];
-                } else if(opcode == 2) {
-                    output = code[code[++i]] * code[code[++i]];
-                } else if(opcode == 99) {
+                if(opcode == Opcode.Add) {
+                    int val1 = (modes[0] == '0') ? memory[memory[++i]] : memory[++i];
+                    int val2 = (modes[1] == '0') ? memory[memory[++i]] : memory[++i];
+                    output = val1 + val2; 
+                } else if(opcode == Opcode.Multiply) {
+                    int val1 = (modes[0] == '0') ? memory[memory[++i]] : memory[++i];
+                    int val2 = (modes[1] == '0') ? memory[memory[++i]] : memory[++i];
+                    output = val1 * val2;
+                } else if(opcode == Opcode.Input) {
+                    output = input.Value;
+                } else if(opcode == Opcode.Output) {
+                    Output.Add((modes[0] == '0') ? memory[memory[++i]] : memory[++i]);
+                    continue; 
+                } else if(opcode == Opcode.Halt) {
                     break;
                 } else {
                     throw new SomethingWentWrongException();
                 }
-                code[code[++i]] = output;
+                memory[memory[++i]] = output;
             }
-            return code;
+            return memory;
         }
 
-        int GetOpcode(int instruction) {
-            if(instruction < 100) return instruction; 
-
-            return instruction; 
+        public int Diagnose() {
+            return Output.Last(); 
         }
+
+        (string modes, Opcode opcode) ParseInstruction(int instruction) {
+            return (
+                instruction.ToString("D5").Remove(3).Reverse(),
+                (Opcode) (instruction % 100)
+            );
+        }
+
+        enum Opcode { Add = 1, Multiply = 2, Input = 3, Output = 4, Halt = 99 }
+        enum Mode { Position, Immediate }
     }
 
     class SomethingWentWrongException : Exception {
