86033e3c61b83a86e551db2e5cf0fa00ed0d14d7
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-06 16:24:35 +0100

    Solved Day 6 Part 2

--- Solutions/Year2019/Day06/Solution.cs
+++ Solutions/Year2019/Day06/Solution.cs
@@ -1,3 +1,4 @@
+using System; 
 using System.Collections.Generic; 
 using System.Linq; 
 
@@ -5,29 +6,46 @@ namespace AdventOfCode.Solutions.Year2019 {
 
     class Day06 : ASolution {
 
+        Dictionary<string, (string direct, List<string> indirect)> orbits; 
+
         public Day06() : base(6, 2019, "Universal Orbit Map") {
 
         }
 
-        protected override string SolvePartOne() {
-            var orbits = new Dictionary<string, (string direct, HashSet<string> indirect)>(); 
+        void InitializeCollection() {
+            if(orbits != null) return; 
+            orbits = new Dictionary<string, (string direct, List<string> indirect)>(); 
             foreach(string s in Input.SplitByNewline()) {
                 string orbiter = s.Substring(s.IndexOf(")") + 1); 
                 string orbitee = s.Substring(0, s.IndexOf(")")); 
-                
-                orbits.Add(orbiter, (orbitee, new HashSet<string>())); 
+                orbits.Add(orbiter, (orbitee, new List<string>())); 
             }
+        }
+
+        List<string> CollectIndirectOrbits(string orbiter) {
+            var list = new List<string>(); 
+            var set = orbits[orbiter].indirect; 
+            for(string o = orbiter; orbits.ContainsKey(o); o = orbits[o].direct) {
+                set.Add(o); 
+                list.Add(o); 
+            }
+            return list; 
+        }
+
+        protected override string SolvePartOne() {
+            InitializeCollection(); 
             foreach(string orbiter in orbits.Keys) {
-                var indirects = orbits[orbiter].indirect; 
-                for(string o = orbiter; orbits.ContainsKey(o); o = orbits[o].direct) {
-                    indirects.Add(o); 
-                }
+                CollectIndirectOrbits(orbiter); 
             }
             return orbits.Sum(x => x.Value.indirect.Count).ToString();
         }
 
         protected override string SolvePartTwo() {
-            return null;
+            InitializeCollection(); 
+            var meList = CollectIndirectOrbits("YOU");
+            var sanList = CollectIndirectOrbits("SAN"); 
+            var both = new List<string>(meList.Concat(sanList));
+            return (both.Count - ((both.Count - new HashSet<string>(both).Count + 1) * 2)).ToString();
         }
     }
 }
