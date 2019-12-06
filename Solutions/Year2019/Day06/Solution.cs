29f9d00c62cfab4a7925fe208520c79740b222ad
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-06 11:45:26 +0100

    Solved Day 6 Part 1

--- Solutions/Year2019/Day06/Solution.cs
+++ Solutions/Year2019/Day06/Solution.cs
@@ -1,13 +1,29 @@
+using System.Collections.Generic; 
+using System.Linq; 
+
 namespace AdventOfCode.Solutions.Year2019 {
 
     class Day06 : ASolution {
 
-        public Day06() : base(6, 2019, "") {
+        public Day06() : base(6, 2019, "Universal Orbit Map") {
 
         }
 
         protected override string SolvePartOne() {
-            return null;
+            var orbits = new Dictionary<string, (string direct, HashSet<string> indirect)>(); 
+            foreach(string s in Input.SplitByNewline()) {
+                string orbiter = s.Substring(s.IndexOf(")") + 1); 
+                string orbitee = s.Substring(0, s.IndexOf(")")); 
+                
+                orbits.Add(orbiter, (orbitee, new HashSet<string>())); 
+            }
+            foreach(string orbiter in orbits.Keys) {
+                var indirects = orbits[orbiter].indirect; 
+                for(string o = orbiter; orbits.ContainsKey(o); o = orbits[o].direct) {
+                    indirects.Add(o); 
+                }
+            }
+            return orbits.Sum(x => x.Value.indirect.Count).ToString();
         }
 
         protected override string SolvePartTwo() {
