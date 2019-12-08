3e70f8f63dd95af371de1ae7e2affdeb471e01ad
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-08 15:53:39 +0100

    Solved Day 8 Part 1

--- Solutions/Year2019/Day08/Solution.cs
+++ Solutions/Year2019/Day08/Solution.cs
@@ -2,12 +2,32 @@ namespace AdventOfCode.Solutions.Year2019 {
 
     class Day08 : ASolution {
 
-        public Day08() : base(8, 2019, "") {
+        public Day08() : base(8, 2019, "Space Image Format") {
 
         }
 
         protected override string SolvePartOne() {
-            return null;
+            var image = Input.ToIntArray().Split(25).Split(6); 
+            int fewest = 0, result = 0; 
+            foreach(var layer in image) {
+                int digit0 = 0, digit1 = 0, digit2 = 0; 
+                foreach(var row in layer) {
+                    foreach(int pixel in row) {
+                        if(pixel == 0) {
+                            digit0++; 
+                        } else if(pixel == 1) {
+                            digit1++; 
+                        } else if(pixel == 2) {
+                            digit2++; 
+                        }
+                    }
+                }
+                if(digit0 < fewest || fewest == 0) {
+                    fewest = digit0; 
+                    result = digit1 * digit2; 
+                }
+            }
+            return result.ToString();
         }
 
         protected override string SolvePartTwo() {
