52a1a0b6f74d52e460482f4c9ce1ee2b27d63ec8
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-12 21:37:31 +0100

    Finished for Day 5

--- Solutions/Year2019/Day02/Solution.cs
+++ Solutions/Year2019/Day02/Solution.cs
@@ -8,14 +8,14 @@ namespace AdventOfCode.Solutions.Year2019 {
             Machine = new NewIntcodeComputer<int>(Input.ToIntArray(","));
         }
 
-        protected override string SolvePartOne() => Machine.Initialize().SetMemory((1,12),(2,2)).Run().Memory[0].ToString();
+        protected override string SolvePartOne() => Machine.Initialize().SetMemory(1, 12).SetMemory(2, 02).Run().Memory[0].ToString();
 
         protected override string SolvePartTwo() {
             int noun = 0; 
             while(true) {
                 int verb = 0;
                 while(verb <= 100) {
-                    if(Machine.Initialize().SetMemory((1, noun), (2, verb)).Run().Memory[0] == 19690720) {
+                    if(Machine.Initialize().SetMemory(1, noun).SetMemory(2, verb).Run().Memory[0] == 19690720) {
                         return (100 * noun + verb).ToString();
                     }
                     verb++;
