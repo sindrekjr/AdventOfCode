2ee983989fa58c3532a88a4cf07c697c5c3be08c
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-12 18:25:43 +0100

    Begun IntcodeComputer overhaul; Day 2 okay

--- Solutions/Year2019/Day02/Solution.cs
+++ Solutions/Year2019/Day02/Solution.cs
@@ -2,21 +2,20 @@ namespace AdventOfCode.Solutions.Year2019 {
 
     class Day02 : ASolution {
 
-        IntcodeComputer<int> Machine;
+        NewIntcodeComputer<int> Machine;
 
         public Day02() : base(2, 2019, "1202 Program Alarm") {
-            Machine = new IntcodeComputer<int>(Input.ToIntArray(","));
+            Machine = new NewIntcodeComputer<int>(Input.ToIntArray(","));
         }
 
-        protected override string SolvePartOne() => Machine.Initialize(12, 2).Run().Memory[0].ToString();
+        protected override string SolvePartOne() => Machine.Initialize().SetMemory((1,12),(2,2)).Run().Memory[0].ToString();
 
         protected override string SolvePartTwo() {
             int noun = 0; 
             while(true) {
                 int verb = 0;
                 while(verb <= 100) {
-                    Machine.Initialize(noun, verb);
-                    if(Machine.Run().Memory[0] == 19690720) {
+                    if(Machine.Initialize().SetMemory((1, noun), (2, verb)).Run().Memory[0] == 19690720) {
                         return (100 * noun + verb).ToString();
                     }
                     verb++;
