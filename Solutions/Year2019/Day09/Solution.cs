4e07a3b8953d6685125763d24b4678e1e1b54435
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-13 00:15:34 +0100

    Solved Day 7 Part 2

--- Solutions/Year2019/Day09/Solution.cs
+++ Solutions/Year2019/Day09/Solution.cs
@@ -8,12 +8,7 @@ namespace AdventOfCode.Solutions.Year2019 {
             Machine = new IntcodeComputer<int>(Input.ToIntArray(","));
         }
 
-        protected override string SolvePartOne() {
-            return Machine.Initialize(1030).WriteInput(1).Run().Output.Dequeue().ToString();
-        }
-
-        protected override string SolvePartTwo() {
-            return null;
-        }
+        protected override string SolvePartOne() => Machine.Initialize(1030).WriteInput(1).Run().Output.Dequeue().ToString();
+        protected override string SolvePartTwo() => Machine.Initialize(1077).WriteInput(2).Run().Output.Dequeue().ToString();
     }
 }
