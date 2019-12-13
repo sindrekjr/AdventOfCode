328b368bb3ac88b761f276cbec2bf3e5eb528701
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-13 09:35:53 +0100

    Solved Day 13 Part 1

--- Solutions/Year2019/Day13/Solution.cs
+++ Solutions/Year2019/Day13/Solution.cs
@@ -2,12 +2,14 @@ namespace AdventOfCode.Solutions.Year2019 {
 
     class Day13 : ASolution {
 
-        public Day13() : base(13, 2019, "") {
+        Arcade Arcade; 
 
+        public Day13() : base(13, 2019, "Care Package") {
+            Arcade = new Arcade(new IntcodeComputer(Input.ToIntArray(","))); 
         }
 
         protected override string SolvePartOne() {
-            return null;
+            return Arcade.Run().GetTileAmount(2).ToString();
         }
 
         protected override string SolvePartTwo() {
