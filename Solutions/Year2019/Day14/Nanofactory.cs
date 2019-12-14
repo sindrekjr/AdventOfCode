c8ccac4148f18bced619b121d86e8c788ef140d9
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-14 11:01:26 +0100

    Moved day-specific implements to their days

---
+++ Solutions/Year2019/Day14/Nanofactory.cs
@@ -0,0 +1,13 @@
+namespace AdventOfCode.Solutions.Year2019 {
+
+    class Nanofactory {
+
+        public Nanofactory(string[] reactions) {
+
+        }
+
+        void ParseReaction(string reaction) {
+            
+        }
+    }
+}
