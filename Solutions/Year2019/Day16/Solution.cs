ef87477974cdbe76a7b7dae3524edd293cd4cb4d
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-16 08:46:01 +0100

    Li'l bit of cleanup but still messy

--- Solutions/Year2019/Day16/Solution.cs
+++ Solutions/Year2019/Day16/Solution.cs
@@ -4,26 +4,24 @@ namespace AdventOfCode.Solutions.Year2019 {
 
     class Day16 : ASolution {
 
-        int[] pattern = {0, 1, 0, -1};
-        int offset; 
-
         public Day16() : base(16, 2019, "Flawed Frequency Transmission") {
-            //DebugInput = "80871224585914546619083218645595";
-            //DebugInput = "03036732577212944063491565474664";
+
         }
 
         protected override string SolvePartOne() => string.Join("", RunPhases(Input.ToIntArray()).Take(8).Select(i => i.ToString()));
 
         protected override string SolvePartTwo() {
-            offset = int.Parse(Input.Substring(0, 7)); 
             int[] rEaLsIgNaL = string.Concat(Enumerable.Repeat(Input, 10000)).ToIntArray(); 
             int[] signalReturn = RunPhases(rEaLsIgNaL); 
+
             string result = ""; 
+            int offset = int.Parse(Input.Substring(0, 7)); 
             for(int i = offset; i < offset + 8; i++) result += signalReturn[i]; 
             return result;
         }
 
         int[] RunPhases(int[] signal, int phases = 100) {
+            int[] pattern = {0, 1, 0, -1};
             for(int phase = 0; phase < 100; phase++) {
                 int[] deconstruct = new int[signal.Length + 1]; 
                 for(int i = 0; i < signal.Length; i++) deconstruct[i + 1] = deconstruct[i] + signal[i];
