d42298c8ae4ce40e63882fec9ab2c8de7da3a9e5
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-16 08:39:39 +0100

    Solved Day 16 Part 2. Yikes.

--- Solutions/Year2019/Day16/Solution.cs
+++ Solutions/Year2019/Day16/Solution.cs
@@ -4,15 +4,26 @@ namespace AdventOfCode.Solutions.Year2019 {
 
     class Day16 : ASolution {
 
+        int[] pattern = {0, 1, 0, -1};
+        int offset; 
+
         public Day16() : base(16, 2019, "Flawed Frequency Transmission") {
             //DebugInput = "80871224585914546619083218645595";
+            //DebugInput = "03036732577212944063491565474664";
         }
 
-        protected override string SolvePartOne() {
-            int[] signal = Input.ToIntArray(); 
-            int[] pattern = {0, 1, 0, -1};            
+        protected override string SolvePartOne() => string.Join("", RunPhases(Input.ToIntArray()).Take(8).Select(i => i.ToString()));
 
+        protected override string SolvePartTwo() {
+            offset = int.Parse(Input.Substring(0, 7)); 
+            int[] rEaLsIgNaL = string.Concat(Enumerable.Repeat(Input, 10000)).ToIntArray(); 
+            int[] signalReturn = RunPhases(rEaLsIgNaL); 
+            string result = ""; 
+            for(int i = offset; i < offset + 8; i++) result += signalReturn[i]; 
+            return result;
+        }
 
+        int[] RunPhases(int[] signal, int phases = 100) {
             for(int phase = 0; phase < 100; phase++) {
                 int[] deconstruct = new int[signal.Length + 1]; 
                 for(int i = 0; i < signal.Length; i++) deconstruct[i + 1] = deconstruct[i] + signal[i];
@@ -38,23 +49,8 @@ namespace AdventOfCode.Solutions.Year2019 {
                     signal[i] = sum; 
                 }
             }
-            /* for(int i = 0; i < signal.Length; i++) {
-                int phase = signal[i]; 
-                foreach(int p in pattern) {
-                    for(int j = 0; j < i + 1; j++) {
-                        if(i == 0 && j == 0) continue; 
-                    }
-                }
-            } */
-            return string.Join("", signal.Take(8).Select(i => i.ToString()));
-        }
 
-        void Phase() {
-            
-        }
-
-        protected override string SolvePartTwo() {
-            return null;
+            return signal; 
         }
     }
 }
