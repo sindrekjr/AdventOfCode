3c79acc116daed682812b762b8bb0a7f4866d63e
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-19 08:08:21 +0100

    Solved Day 19 Part 1

--- Solutions/Year2019/Day19/Solution.cs
+++ Solutions/Year2019/Day19/Solution.cs
@@ -2,12 +2,21 @@ namespace AdventOfCode.Solutions.Year2019 {
 
     class Day19 : ASolution {
 
-        public Day19() : base(19, 2019, "") {
+        IntcodeComputer Drone; 
 
+        public Day19() : base(19, 2019, "") {
+            Drone = new IntcodeComputer(Input.ToIntArray(",")); 
+            //Drone.Initialize(5000); 
         }
 
         protected override string SolvePartOne() {
-            return null;
+            int affected = 0; 
+            for(int x = 0; x < 50; x++) {
+                for(int y = 0; y < 50; y++) {
+                    if(Drone.Initialize(500).WriteInput(x, y).Run().ReadOutput() == 1) affected++; 
+                }
+            }
+            return affected.ToString();
         }
 
         protected override string SolvePartTwo() {
