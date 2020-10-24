0189b796877f4574718dfdaa97d6a95d0165fb68
Sindre S. Kjær <sindre@sindrekjr.no>
2020-10-24 16:20:03 +0200

    Pulled changes from template project

---
+++ AdventOfCode/Solutions/Year2019/Day01/Solution.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+
+namespace AdventOfCode.Solutions.Year2019
+{
+
+    class Day01 : ASolution
+    {
+
+        public Day01() : base(1, 2019, "The Tyranny of the Rocket Equation")
+        {
+
+        }
+
+        protected override string SolvePartOne() => Input.ToIntArray("\n").Select(Fuel).Sum().ToString();
+
+        protected override string SolvePartTwo() => Input.ToIntArray("\n").Select(FuelFuel).Sum().ToString();
+
+        int Fuel(int module) => module / 3 - 2;
+
+        int FuelFuel(int module)
+        {
+            int fuel = Fuel(module);
+            return fuel <= 0 ? 0 : fuel + FuelFuel(fuel);
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/Year2019/Day19/Solution.cs
@@ -0,0 +1,40 @@
+namespace AdventOfCode.Solutions.Year2019
+{
+
+    class Day19 : ASolution
+    {
+
+        IntcodeComputer Drone;
+
+        public Day19() : base(19, 2019, "Tractor Beam")
+        {
+            Drone = new IntcodeComputer(Input.ToIntArray(","));
+        }
+
+        protected override string SolvePartOne()
+        {
+            int affected = 0;
+            for(int x = 0; x < 50; x++)
+            {
+                for(int y = 0; y < 50; y++)
+                {
+                    if(Drone.Initialize(500).WriteInput(x, y).Run().ReadOutput() == 1) affected++;
+                }
+            }
+            return affected.ToString();
+        }
+
+        protected override string SolvePartTwo()
+        {
+            int x = 0;
+            for(int y = 1000; ; y++)
+            {
+                while(Drone.Initialize(500).WriteInput(x, y).Run().ReadOutput() == 0) x++;
+                if(Drone.Initialize(500).WriteInput(x + 99, y - 99).Run().ReadOutput() == 1)
+                {
+                    return ((x * 10000) + (y - 99)).ToString();
+                }
+            }
+        }
+    }
+}
