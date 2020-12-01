d025e1686e6a5fcfd222d19d50a0d3bf148b6b52
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-01 06:51:04 +0100

    Solved Day 1

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -8,18 +8,44 @@ namespace AdventOfCode.Solutions.Year2020
     class Day01 : ASolution
     {
 
-        public Day01() : base(01, 2020, "")
-        {
+        List<int> Expenses = new List<int>();
 
+        public Day01() : base(01, 2020, "Report Repair")
+        {
+            foreach (var expense in Input.SplitByNewline()) 
+            {
+                Expenses.Add(int.Parse(expense));
+            }
         }
 
         protected override string SolvePartOne()
         {
+            foreach (var expense in Expenses)
+            {
+                if (expense > 2020) continue;
+                foreach (var exp in Expenses)
+                {
+                    if (exp > 2020) continue;
+                    if (expense + exp == 2020) return (expense * exp).ToString();
+                }
+            }
             return null;
         }
 
         protected override string SolvePartTwo()
         {
+            foreach (var expense in Expenses)
+            {
+                if (expense > 2020) continue;
+                foreach (var exp1 in Expenses)
+                {
+                    if (exp1 > 2020) continue;
+                    foreach (var exp2 in Expenses)
+                    {
+                        if (expense + exp1 + exp2 == 2020) return (expense * exp1 * exp2).ToString();
+                    }
+                }
+            }
             return null;
         }
     }
