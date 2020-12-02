3924611f34896cf0b3633acc7d7692102111dfc1
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-02 08:33:38 +0100

    Solved Day 2 Part 2

--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -20,23 +20,44 @@ namespace AdventOfCode.Solutions.Year2020
             int valid = 0;
             foreach(var pass in Passwords)
             {
-                if (isValid(pass[0], pass[1])) valid++;
+                if (isValid_Deprecated(pass[0], pass[1])) valid++;
             }
             return valid.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int valid = 0;
+            foreach(var pass in Passwords)
+            {
+                if (isValid(pass[0], pass[1])) valid++;
+            }
+            return valid.ToString();
         }
 
-        bool isValid(string policy, string password)
+        bool isValid_Deprecated(string policy, string password)
         {
             var (rule, pattern, _) = policy.Split(" ");
             var (lo, hi, _) = rule.Split("-");
             
             var count = (password.Length - password.Replace(pattern, "").Length) / pattern.Length;
             return count >= int.Parse(lo) && count <= int.Parse(hi);
+        }
+
+        bool isValid(string policy, string password)
+        {
+            var (rule, pattern, _) = policy.Split(" ");
+            var (lo, hi, _) = rule.Split("-");
+
+            int i = 0;
+            int count = 0;
+            while ((i = password.IndexOf(pattern, i)) != -1)
+            {
+                if ((i + 1) == int.Parse(lo) || (i + 1) == int.Parse(hi)) count++;
+                i += pattern.Length;
+            }
+
+            return count == 1;
         }
     }
 }
