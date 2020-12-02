d67cecea27bd8c9cba4ce7d1799f3bb8438dfd58
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-02 08:23:12 +0100

    Solved Day 2 Part 1

--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,20 +8,35 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day02 : ASolution
     {
+        string[][] Passwords;
 
-        public Day02() : base(02, 2020, "")
+        public Day02() : base(02, 2020, "Password Philosophy")
         {
-
+            Passwords = Input.SplitByNewline().Select(str => str.Split(": ")).ToArray();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            int valid = 0;
+            foreach(var pass in Passwords)
+            {
+                if (isValid(pass[0], pass[1])) valid++;
+            }
+            return valid.ToString();
         }
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        bool isValid(string policy, string password)
+        {
+            var (rule, pattern, _) = policy.Split(" ");
+            var (lo, hi, _) = rule.Split("-");
+            
+            var count = (password.Length - password.Replace(pattern, "").Length) / pattern.Length;
+            return count >= int.Parse(lo) && count <= int.Parse(hi);
         }
     }
 }
