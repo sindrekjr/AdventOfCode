22a376347f40a8a4854130ca3bb51bc5b2b22784
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-03 06:55:19 +0100

    Solved Day 3 Part 2

--- AdventOfCode/Solutions/Year2020/Day03/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day03/Solution.cs
@@ -16,31 +16,12 @@ namespace AdventOfCode.Solutions.Year2020
             Map = Input.SplitByNewline().Select(line => line.Select(c => c == '#').ToArray()).ToArray();
         }
 
-        protected override string SolvePartOne()
-        {
-            int x = 0;
-            int count = 0;
-            return CountSlopeTrees(1, 3).ToString();
-            for (int y = 0; y < Map.Length; y++, x += 3)
-            {
-                if (x >= Map[y].Length) x -= Map[y].Length;
-                if (Map[y][x]) count++;
-
-                /*string line = "";
-                foreach (var node in Map[y]) line += node ? "#" : ".";
-                var sb = new StringBuilder(line);
-                sb[x] = sb[x] == '#' ? 'X' : 'O';
-                Console.WriteLine($"{sb.ToString()} -- {count}");*/
-            }
-            return count.ToString();
-        }
+        protected override string SolvePartOne() => CountSlopeTrees(1, 3).ToString();
 
         protected override string SolvePartTwo()
-        {
-            return (CountSlopeTrees(1, 1) * CountSlopeTrees(3, 1) * CountSlopeTrees(5, 1) * CountSlopeTrees(7, 1) * CountSlopeTrees(1, 2)).ToString();
-        }
+            => (CountSlopeTrees(1, 1) * CountSlopeTrees(1, 3) * CountSlopeTrees(1, 5) * CountSlopeTrees(1, 7) * CountSlopeTrees(2, 1)).ToString();
 
-        BigInteger CountSlopeTrees(int down, int right)
+        long CountSlopeTrees(int down, int right)
         {
             int x = 0;
             int count = 0;
