088dbb37389d43ba0bcd0f3f5f507ca8d5d155e5
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-04 07:49:14 +0100

    Solved Day 4 Part 1

--- AdventOfCode/Solutions/Year2020/Day04/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day04/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,20 +8,46 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day04 : ASolution
     {
+        Dictionary<string, string>[] Passports;
 
-        public Day04() : base(04, 2020, "")
+        public Day04() : base(04, 2020, "Passport Processing")
         {
-
+            Passports = Input.Split("\n\n").Select(pass => {
+                var dict = new Dictionary<string, string>();
+                var fields = pass.Replace(" ", "\n").SplitByNewline();
+                foreach (var field in fields)
+                {
+                    var (k, v, _) = field.Split(":");
+                    dict.Add(k, v);
+                }
+                return dict;
+            }).ToArray();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var requiredFields = new string[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+            int count = 0;
+            foreach (var passport in Passports)
+            {
+                if (ContainsAll(passport, requiredFields)) count++;
+            }
+            
+            return count.ToString();
         }
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        bool ContainsAll(Dictionary<string, string> dict, string[] fields)
+        {
+            foreach (var field in fields)
+            {
+                if (!dict.ContainsKey(field)) return false;
+            }
+            return true;
         }
     }
 }
