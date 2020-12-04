a452ed8fa1a37b063e6bb1e7f930a568aa2fdfb6
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-04 15:34:58 +0100

    Solved Day 4 Part 2

--- AdventOfCode/Solutions/Year2020/Day04/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day04/Solution.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Solutions.Year2020
 {
@@ -9,6 +10,8 @@ namespace AdventOfCode.Solutions.Year2020
     class Day04 : ASolution
     {
         Dictionary<string, string>[] Passports;
+        string[] RequiredFields = new string[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+        string[] ValidHairColours = new string[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
 
         public Day04() : base(04, 2020, "Passport Processing")
         {
@@ -25,29 +28,51 @@ namespace AdventOfCode.Solutions.Year2020
         }
 
         protected override string SolvePartOne()
-        {
-            var requiredFields = new string[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
-            int count = 0;
-            foreach (var passport in Passports)
-            {
-                if (ContainsAll(passport, requiredFields)) count++;
-            }
-            
-            return count.ToString();
-        }
+            => Passports.Count(ContainsRequiredFields).ToString();
 
         protected override string SolvePartTwo()
-        {
-            return null;
-        }
+            => Passports.Count(Validate).ToString();
 
-        bool ContainsAll(Dictionary<string, string> dict, string[] fields)
+        bool ContainsRequiredFields(Dictionary<string, string> dict)
         {
-            foreach (var field in fields)
+            foreach (var field in RequiredFields)
             {
                 if (!dict.ContainsKey(field)) return false;
             }
             return true;
+        }
+
+        bool Validate(Dictionary<string, string> dict)
+        {
+            try
+            {
+                var byr = int.Parse(dict["byr"]);
+                if (byr < 1920 || 2002 < byr) return false;
+
+                var iyr = int.Parse(dict["iyr"]);
+                if (iyr < 2010 || 2020 < iyr) return false;
+
+                var eyr = int.Parse(dict["eyr"]);
+                if (eyr < 2020 || 2030 < eyr) return false;
+
+                var hgt = dict["hgt"].SplitAtIndex(dict["hgt"].Length - 2);
+                var hgtN = int.Parse(hgt[0]);
+                var hgtM = hgt[1];
+                if (hgtM == "cm") if (hgtN < 150 || 193 < hgtN) return false;
+                if (hgtM == "in") if (hgtN < 59 || 76 < hgtN) return false;
+
+                var hcl = dict["hcl"];
+                if (!new Regex("#[a-z0-9]{6}").IsMatch(hcl)) return false;
+
+                var ecl = dict["ecl"];
+                if (!ValidHairColours.Contains(ecl)) return false;
+
+                var pid = dict["pid"];
+                if (pid.Length != 9 || !int.TryParse(pid, out int dummie)) return false;
+
+                return true;
+            }
+            catch { return false; }
         }
     }
 }
