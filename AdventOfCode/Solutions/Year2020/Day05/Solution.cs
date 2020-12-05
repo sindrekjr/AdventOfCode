5d6a43d855858237e99e33d357839e07e6b1f1c5
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-05 12:10:53 +0100

    Solved Day 5 Part 1

--- AdventOfCode/Solutions/Year2020/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day05/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -8,19 +9,39 @@ namespace AdventOfCode.Solutions.Year2020
     class Day05 : ASolution
     {
 
-        public Day05() : base(05, 2020, "")
+        public Day05() : base(05, 2020, "Binary Boarding")
         {
 
         }
 
         protected override string SolvePartOne()
-        {
-            return null;
-        }
+            => Input.SplitByNewline().Aggregate(0, (hi, bpass) => 
+            {
+                var (row, col) = GetSeat(bpass);
+                var id = (row * 8) + col;
+                return id > hi ? id : hi;
+            }).ToString();
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        (int row, int col) GetSeat(string bpass)
+        {
+            double front = 0;
+            double back = 127;
+            double left = 0;
+            double right = 7;
+            foreach (var ch in bpass)
+            {
+                if (ch == 'F') back -= Math.Ceiling((back - front) / 2);
+                if (ch == 'B') front += Math.Ceiling((back - front) / 2);
+                if (ch == 'L') right -= Math.Ceiling((right - left) / 2);
+                if (ch == 'R') left += Math.Ceiling((right - left) / 2);
+            }
+
+            return ((int) front, (int) left);
         }
     }
 }
