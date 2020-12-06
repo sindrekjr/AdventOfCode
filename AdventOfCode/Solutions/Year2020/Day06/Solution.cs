dbd328f79ec369380242eee0a60e4cfc77608c56
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-06 12:43:52 +0100

    Solved Day 6

--- AdventOfCode/Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -8,19 +9,12 @@ namespace AdventOfCode.Solutions.Year2020
     class Day06 : ASolution
     {
 
-        public Day06() : base(06, 2020, "")
-        {
-
-        }
+        public Day06() : base(06, 2020, "Custom Customs") { }
 
         protected override string SolvePartOne()
-        {
-            return null;
-        }
+            => Input.Split("\n\n").Aggregate(0, (count, group) => count + group.Replace("\n", "").ToHashSet().Count).ToString();
 
         protected override string SolvePartTwo()
-        {
-            return null;
-        }
+            => Input.Split("\n\n").Aggregate(0, (count, group) => count + group.SplitByNewline().IntersectAll().Count()).ToString();
     }
 }
