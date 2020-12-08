d503f6b99843820d1f93b58ac9ed0cc2f1347aa3
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-08 07:23:04 +0100

    Solved Day 8 Part 1

--- AdventOfCode/Solutions/Year2020/Day08/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day08/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -8,14 +9,30 @@ namespace AdventOfCode.Solutions.Year2020
     class Day08 : ASolution
     {
 
-        public Day08() : base(08, 2020, "")
+        public Day08() : base(08, 2020, "Handheld Halting")
         {
 
         }
 
+        (string cmd, int n, bool visited)[] GetInstructions() => Input.SplitByNewline().Select(l => 
+        {
+            var (inst, val, _) = l.Split(" ");
+            return (inst, int.Parse(val.Contains("+") ? val.Substring(1) : val), false);
+        }).ToArray();
+
         protected override string SolvePartOne()
         {
-            return null;
+            int acc = 0;
+            var instructions = GetInstructions();
+            for (int i = 0;;)
+            {
+                if (instructions[i].visited) return acc.ToString();
+
+                instructions[i].visited = true;
+                if (instructions[i].cmd == "acc") acc += instructions[i++].n;
+                if (instructions[i].cmd == "jmp") i += instructions[i].n;
+                if (instructions[i].cmd == "nop") i++;
+            }
         }
 
         protected override string SolvePartTwo()
