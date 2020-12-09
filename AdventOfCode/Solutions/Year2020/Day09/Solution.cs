c00a6bae65dab81d629195a8f515a275bcd30c13
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-09 12:05:03 +0100

    Solved Day 9 Part 1

--- AdventOfCode/Solutions/Year2020/Day09/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day09/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -8,19 +9,44 @@ namespace AdventOfCode.Solutions.Year2020
     class Day09 : ASolution
     {
 
-        public Day09() : base(09, 2020, "")
+        public Day09() : base(09, 2020, "Encoding Error")
         {
 
         }
 
         protected override string SolvePartOne()
         {
+            var sequence = ParseXMAS();
+            for (int i = 25; i < sequence.Length; i++)
+            {
+                int value = sequence[i];
+                if (!IsSumOfPreamble(sequence.Skip(i - 25).Take(25), value))
+                {
+                    return value.ToString();
+                }
+            }
             return null;
         }
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        int[] ParseXMAS() => Input.ToIntArray("\n");
+
+        bool IsSumOfPreamble(IEnumerable<int> preamble, int sum)
+        {
+            var queue = new Queue<int>(preamble);
+            while (queue.Any())
+            {
+                var n1 = queue.Dequeue();
+                foreach (var n2 in queue)
+                {
+                    if (n1 + n2 == sum) return true;
+                }
+            }
+            return false;
         }
     }
 }
