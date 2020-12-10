e51106ee0ce966256c56d6d5b06b4c53653d44a6
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-10 19:51:50 +0100

    Pulled changes from base repository

--- AdventOfCode/Infrastructure/Models/SolutionResult.cs
+++ AdventOfCode/Infrastructure/Models/SolutionResult.cs
@@ -6,5 +6,7 @@ namespace AdventOfCode.Infrastructure.Models
     {
         public string Answer { get; set; }
         public TimeSpan Time { get; set; }
+
+        public static SolutionResult Empty => new SolutionResult();
     }
 }
