c8d5a5f4883ef82207df82c2d1249bd4c6ae2d68
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-12 19:36:03 +0100

    Cleanup Day 11

--- AdventOfCode/Solutions/Year2020/Day11/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day11/Solution.cs
@@ -5,50 +5,42 @@ using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
     class Day11 : ASolution
     {
-
-        public Day11() : base(11, 2020, "Seating System", true) { }
+        public Day11() : base(11, 2020, "Seating System") { }
 
         protected override string SolvePartOne()
         {
-            var seating = GetSeatingArrangementDict();
+            var seating = GetSeatingArrangement();
             bool stable = false;
             int occupied = 0;
             while (!stable)
             {
-                (seating, occupied, stable) = ProcessSeatingRoundV2(seating, 4);
-
-                Console.WriteLine($"Occupied: {occupied}");
-                PrintSeatingArrangementV2(seating);
+                (seating, occupied, stable) = ProcessSeatingRound(seating, 4, false);
             }
 
             return occupied.ToString();
-
-            // var seating = GetSeatingArrangement();
-            // bool stable = false;
-            // int occupied = 0;
-            // while (!stable)
-            // {
-            //     (seating, occupied, stable) = ProcessSeatingRound(seating, 4);
-            // }
-
-            // return occupied.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var seating = GetSeatingArrangement();
+            bool stable = false;
+            int occupied = 0;
+            while (!stable)
+            {
+                (seating, occupied, stable) = ProcessSeatingRound(seating, 5);
+            }
+
+            return occupied.ToString();
         }
 
-        Tile[][] GetSeatingArrangement()
-            => Input.SplitByNewline().Select(row => row.Select(c => new Tile { Seat = c == 'L' }).ToArray()).ToArray();
+        Map<Tile> GetSeatingArrangement()
+            => new Map<Tile>(
+                Input.SplitByNewline()
+                    .Select(row => row.Select(c => c == 'L' ? Tile.Seat : Tile.Empty).ToArray()).ToArray());
 
-        Map<Tile> GetSeatingArrangementDict()
-            => new Map<Tile>(GetSeatingArrangement());
-
-        (Map<Tile>, int occupied, bool stable) ProcessSeatingRoundV2(Map<Tile> original, int preference)
+        (Map<Tile>, int occupied, bool stable) ProcessSeatingRound(Map<Tile> original, int preference, bool sight = true)
         {
             var map = new Map<Tile>();
 
@@ -56,17 +48,22 @@ namespace AdventOfCode.Solutions.Year2020
             bool stable = true;
             foreach (var key in original.Keys)
             {
-                var tile = (Tile) original[key].Clone();
+                var tile = original[key];
                 map.Add(key, tile);
-                if(!tile.Seat) continue;
+                if(tile == Tile.Empty) continue;
 
-                var adjacents = original.PeekAround(key, 1).Aggregate(0, (acc, direction) => 
+                var adjacents = original.PeekAround(key, sight ? -1 : 1).Aggregate(0, (acc, direction) => 
                 {
-                    if (direction.Count() == 0) return acc;
-                    return direction.First().Occupied ? acc + 1 : acc;
+                    foreach (var t in direction)
+                    {
+                        if (t == Tile.Occupied) return acc + 1;
+                        if (t == Tile.Seat) return acc;
+                    }
+
+                    return acc;
                 });
 
-                if (tile.Occupied)
+                if (tile == Tile.Occupied)
                 {
                     if (adjacents < preference)
                     {
@@ -75,11 +72,11 @@ namespace AdventOfCode.Solutions.Year2020
                     }
 
                     stable = false;
-                    tile.Occupied = false;
+                    map[key] = Tile.Seat;
                 }
                 else if (adjacents == 0)
                 {
-                    tile.Occupied = true;
+                    map[key] = Tile.Occupied;
                     occupied++;
                     stable = false;
                 }
@@ -87,105 +84,7 @@ namespace AdventOfCode.Solutions.Year2020
 
             return (map, occupied, stable);
         }
-
-        (Tile[][] tiles, int occupied, bool stable) ProcessSeatingRound(Tile[][] tiles, int preference)
-        {
-            var original = tiles.Select(r => r.Select(t => (Tile)t.Clone()).ToArray()).ToArray();
-
-            int occupied = 0;
-            bool stable = true;
-            int rows = tiles.Length;
-            int cols = tiles[0].Length;
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    var seat = original[i][j];
-                    if (!seat.Seat) continue;
-
-                    if (seat.Occupied)
-                    {
-                        if (seat.AdjacentOccupied < preference)
-                        {
-                            occupied++;
-                            continue;
-                        }
-
-                        stable = false;
-                        tiles[i][j].Occupied = false;
-                        foreach (var (x, y) in GetAdjacentTiles((i, j), rows, cols))
-                        {
-                            if (tiles[x][y].Seat) tiles[x][y].AdjacentOccupied--;
-                        }
-                    }
-                    else if (seat.AdjacentOccupied == 0)
-                    {
-                        occupied++;
-                        stable = false;
-                        tiles[i][j].Occupied = true;
-                        foreach (var (x, y) in GetAdjacentTiles((i, j), rows, cols))
-                        {
-                            if (tiles[x][y].Seat) tiles[x][y].AdjacentOccupied++;
-                        }
-                    }
-                }
-            }
-
-            return (tiles, occupied, stable);
-        }
-
-        IEnumerable<(int x, int y)> GetAdjacentTiles((int x, int y) tile, int rows, int cols)
-        {
-            var (x, y) = tile;
-            for (int i = x == 0 ? x : x - 1; i < rows && i < x + 2; i++)
-            {
-                for (int j = y == 0 ? y : y - 1; j >= 0 && j < cols && j < y + 2; j++)
-                {
-                    if (i == x && j == y) continue;
-                    yield return (i, j);
-                }
-            }
-        }
-
-        void PrintSeatingArrangement(Tile[][] seats)
-        {
-            foreach (var x in seats)
-            {
-                foreach (var y in x)
-                {
-                    if (y.Occupied) Console.Write("#");
-                    else if (y.Seat) Console.Write("L");
-                    else Console.Write(".");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        void PrintSeatingArrangementV2(Map<Tile> tiles)
-        {
-            string[][] output = new string[10][].Select(t => new string[10]).ToArray();
-
-            foreach (var key in tiles.Keys)
-            {
-                output[key.x][key.y] = tiles[key].Occupied ? "#" : tiles[key].Seat ? "L" : ".";
-            }
-
-            foreach (var str in output) Console.WriteLine(str.JoinAsStrings());
-            Console.WriteLine();
-        }
     }
 
-    internal class Tile : ICloneable
-    {
-        public bool Seat { get; set; }
-        public bool Occupied { get; set; }
-        public int AdjacentOccupied { get; set; }
-
-        public object Clone() => new Tile
-        {
-            Seat = this.Seat,
-            Occupied = this.Occupied,
-            AdjacentOccupied = this.AdjacentOccupied,
-        };
-    }
+    internal enum Tile { Empty, Seat, Occupied }
 }
