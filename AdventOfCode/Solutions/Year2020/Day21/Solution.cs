900d9fde0ab61f549889ad961d00e1f0512039eb
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-21 13:54:42 +0100

    Solved Day 21.1

--- AdventOfCode/Solutions/Year2020/Day21/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day21/Solution.cs
@@ -1,26 +1,64 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
     class Day21 : ASolution
     {
+        Dictionary<string, int> Ingredients;
+        Dictionary<string, HashSet<string>> ImplicatedIngredients;
 
-        public Day21() : base(21, 2020, "")
-        {
-
-        }
+        public Day21() : base(21, 2020, "Allergen Assessment") { }
 
         protected override string SolvePartOne()
         {
-            return null;
+            Ingredients = new Dictionary<string, int>();
+            ImplicatedIngredients = new Dictionary<string, HashSet<string>>();
+
+            foreach (var (ingredients, allergens) in GetFoods())
+            {
+                foreach(var i in ingredients)
+                {
+                    if (Ingredients.ContainsKey(i))
+                    {
+                        Ingredients[i]++;
+                    }
+                    else
+                    {
+                        Ingredients.Add(i, 1);
+                    }
+                }
+
+                foreach (var a in allergens)
+                {
+                    if (!ImplicatedIngredients.ContainsKey(a))
+                    {
+                        ImplicatedIngredients.Add(a, new HashSet<string>(ingredients));
+                    }
+                    else
+                    {
+                        ImplicatedIngredients[a] = ImplicatedIngredients[a].Intersect(ingredients).ToHashSet();
+                    }
+                }
+            }
+
+            var allImplicatedIngredients = ImplicatedIngredients.Values.SelectMany(s => s).ToHashSet();
+
+            return Ingredients.Aggregate(0, (count, i) => allImplicatedIngredients.Contains(i.Key) ? count : count + i.Value).ToString();
         }
 
         protected override string SolvePartTwo()
         {
             return null;
         }
+
+        (string[] ingredients, string[] allergens)[] GetFoods()
+            => Input.SplitByNewline().Select(l =>
+            {
+                var (ing, alg, _) = l.Split(" (contains ");
+                return (ing.Split(" "), alg.TrimEnd(')').Split(", "));
+            }).ToArray();
     }
 }
