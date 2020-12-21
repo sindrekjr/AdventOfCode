f61a2c9078851ac581a876380d7bfadee207d8a7
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-21 22:53:09 +0100

    Solved Day 19.2

--- AdventOfCode/Solutions/Year2020/Day19/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day19/Solution.cs
@@ -15,21 +15,32 @@ namespace AdventOfCode.Solutions.Year2020
         protected override string SolvePartOne()
         {
             var (rules, messages, _) = Input.SplitByParagraph();
-            Rules = new Dictionary<int, string[]>();
-            foreach (var line in rules.SplitByNewline())
-            {
-                var (key, value, _) = line.Split(": ");
-                Rules.Add(int.Parse(key), value.Contains('|')
-                    ? value.Split(" | ")
-                    : new string[] { value.Trim('"') });
-            }
-
+            Rules = MapRulesToDictionary(rules.SplitByNewline());
             return messages.SplitByNewline().Count(m => Regex.IsMatch(m, $"^{ParseRule(0)}$")).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var (rules, messages, _) = Input.SplitByParagraph();
+            
+            Rules = MapRulesToDictionary(rules.SplitByNewline());
+            Rules[8] = new string[] { "42", "42 8" };
+            Rules[11] = new string[] { "42 31", "42 11 31" };
+
+            return messages.SplitByNewline().Count(m => Regex.IsMatch(m, $"^{ParseRule(0)}$")).ToString();
+        }
+
+        Dictionary<int, string[]> MapRulesToDictionary(string[] rules)
+        {
+            var dict = new Dictionary<int, string[]>();
+            foreach (var r in rules)
+            {
+                var (key, value, _) = r.Split(": ");
+                dict.Add(int.Parse(key), value.Contains('|')
+                    ? value.Split(" | ")
+                    : new string[] { value.Trim('"') });
+            }
+            return dict;
         }
 
         string ParseRule(int i)
@@ -42,10 +53,26 @@ namespace AdventOfCode.Solutions.Year2020
             }
             else
             {
-                var alt = rule.Select(r => r.Split(" ").Select(n => ParseRule(int.Parse(n))).ToArray()).ToArray();
+                var alt = rule.Select(r => r.Split(" ").Select(n => ParseRule(int.Parse(n))));
+
+                if (i == 8 && rule.Contains("42 8"))
+                {
+                    alt = rule.Take(1).Select(r => r.Split(" ").Select(n => $"{ParseRule(int.Parse(n))}+"));
+                }
+
+                if (i == 11 && rule.Contains("42 11 31"))
+                {
+                    alt = rule.Skip(1).Select(r => r.Split(" ").Select(n => 
+                    {
+                        var r = int.Parse(n);
+                        if (r == 42) return $"(?<el>{ParseRule(r)})+";
+                        if (r == 11) return $"(?<th-el>{ParseRule(31)})+";
+                        return "(?(el)(?!))";
+                    }));
+                }
+                
                 return "(" + string.Join('|', alt.Select(n => n.JoinAsStrings())) + ")";
             }
         }
-
     }
 }
