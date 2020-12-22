7c1d0a9581b38ee40ba07c875517d5263d7e9fc2
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-22 17:29:29 +0100

    Attempt improve Day 18

--- AdventOfCode/Solutions/Year2020/Day18/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day18/Solution.cs
@@ -8,50 +8,72 @@ namespace AdventOfCode.Solutions.Year2020
 {
     class Day18 : ASolution
     {
-        public Day18() : base(18, 2020, "Operation Order") { }
+        public Day18() : base(18, 2020, "Operation Order", true) { }
 
         protected override string SolvePartOne()
             => Input.Replace(" ", "").SplitByNewline().Aggregate(default(long), (acc, exp) => acc + Resolve(exp)).ToString();
 
         protected override string SolvePartTwo()
-        {
-            var input = Input.Replace(" ", "");
-            input = Regex.Replace(input, "([0-9+]+)[+]([0-9+]+)", "($1+$2)");
-            input = Regex.Replace(input, "[*]([0-9][+].*?[)])", "*($1)");
+            => Input.Replace(" ", "").SplitByNewline().Aggregate(default(long), (acc, exp) => acc + Resolve(exp, '+')).ToString();
 
-            Console.WriteLine(input);
-
-            return input.SplitByNewline().Take(1).Aggregate(default(long), (acc, exp) => acc + Resolve(exp)).ToString();
-        }
-        long Resolve(string expression)
+        long Resolve(string expression, char precedence = default)
         {
             long sum = 0;
             char operand = default;
+            List<long> storage = new List<long>();
+
             for (int i = 0; i < expression.Length; i++)
             {
                 var c = expression[i];
                 if (c == '(')
                 {
                     var end = Utilities.IndexOfClosingParenthesis(expression, i) - 1;
-                    var n = Resolve(expression[(i + 1)..(end)]);
+                    var n = Resolve(expression[(i + 1)..(end)], precedence);
                     i = end;
-                    
-                    sum = operand == default
-                        ? n
-                        : operand == '+' ? sum + n : sum * n;
+
+                    if (operand == default)
+                    {
+                        sum = n;
+                    }
+                    else if (precedence != default && operand != precedence)
+                    {
+                        storage.Add(n);
+                    }
+                    else
+                    {
+                        sum = operand == '+' ? sum + n : sum * n;
+                    }
                 }
                 else if (int.TryParse(c.ToString(), out int n))
                 {
-                    sum = operand == default
-                        ? n
-                        : operand == '+' ? sum + n : sum * n;
+                    if (operand == default)
+                    {
+                        sum = n;
+                    }
+                    else if (precedence != default && operand != precedence)
+                    {
+                        expression = expression.Substring(0, i) + '(' + expression.Substring(i);
+                        var end = expression.IndexOf(')', i);
+                        expression = end == -1
+                            ? expression + ')'
+                            : expression.Substring(0, end) + ')' + expression.Substring(end);
+
+                        i--;
+                    }
+                    else
+                    {
+                        sum = operand == '+' ? sum + n : sum * n;
+                    }
                 }
                 else
                 {
                     operand = c;
                 }
             }
-            return sum;
+
+            return precedence == default
+                ? sum
+                : storage.Aggregate(sum, (acc, n) => precedence == '+' ? acc * n : acc + n);
         }
     }
 }
