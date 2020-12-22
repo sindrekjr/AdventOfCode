e9f0eadf4b53baa9ce48ebb2b3b2a60cf734d098
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-22 23:21:42 +0100

    Started assembling Day 20.2

--- AdventOfCode/Solutions/Year2020/Day20/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day20/Solution.cs
@@ -8,6 +8,8 @@ namespace AdventOfCode.Solutions.Year2020
 {
     class Day20 : ASolution
     {
+        int Aspect;
+        Map<ImageTile> Puzzle;
         Dictionary<int, ImageTile> Tiles;
 
         public Day20() : base(20, 2020, "Jurassic Jigsaw") { }
@@ -28,8 +30,56 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
-            return null;
+            Tiles = new Dictionary<int, ImageTile>();
+            foreach (var (title, tile) in Input.SplitByParagraph().Select(p => p.SplitByNewline()))
+            {
+                var id = int.Parse(title.Substring(5, 4));
+                Tiles.Add(id, ParseTile(id, tile.Take(tile.Count - 1).Skip(1).Select(s => s.Substring(1, s.Length - 2))));
+            }
+            Aspect = (int) Math.Sqrt(Tiles.Count);
+
+            foreach (var tile in Tiles.Values) FindMatches(tile);
+
+            CreatePuzzle();
+            return Puzzle.Count.ToString();
         }
+
+        void CreatePuzzle()
+        {
+            Puzzle = new Map<ImageTile>();
+            var placedTile = Tiles.Values.Where(IsCornerTile).First(t => PlaceCornerTile(t, Aspect));
+            var position = Puzzle.Keys.First();
+
+            while (Tiles.Count > 0) position = PlaceNextTile(position);
+
+            // Console.WriteLine("Left: " + placedTile.LeftMatches.Where(IsEdgeTile).Count());
+            // Console.WriteLine("Bottom: " + placedTile.BottomMatches.Where(IsEdgeTile).Count());
+            // Console.WriteLine((placedTile == Puzzle[(0, Aspect)]).ToString());
+            // Console.WriteLine(Puzzle.Count);
+        }
+
+        (int, int) PlaceNextTile((int x, int y) position)
+        {
+            var previousTile = Puzzle[position];
+            if (IsCornerTile(previousTile))
+            {
+                Console.WriteLine("Top: " + previousTile.TopMatches.Where(IsEdgeTile).Count());
+                Console.WriteLine("Right: " + previousTile.RightMatches.Where(IsEdgeTile).Count());
+                Console.WriteLine("Bottom: " + previousTile.BottomMatches.Where(IsEdgeTile).Count());
+                Console.WriteLine("Left: " + previousTile.LeftMatches.Where(IsEdgeTile).Count());
+            }
+
+            Tiles.Remove(Tiles.Keys.First(), out ImageTile next);
+            return position;
+        }
+
+        bool PlaceCornerTile(ImageTile tile, int sqrt) =>
+        (
+            (tile.RightMatches.Any() && tile.BottomMatches.Any() && Puzzle.TryAdd((0, 0), tile))
+            || (tile.RightMatches.Any() && tile.TopMatches.Any() && Puzzle.TryAdd((sqrt, 0), tile))
+            || (tile.LeftMatches.Any() && tile.BottomMatches.Any() && Puzzle.TryAdd((0, sqrt), tile))
+            || (tile.LeftMatches.Any() && tile.TopMatches.Any() && Puzzle.TryAdd((sqrt, sqrt), tile))
+        ) && Tiles.Remove(tile.Id);
 
         ImageTile ParseTile(int id, IEnumerable<string> tile)
             => new ImageTile
@@ -61,6 +111,9 @@ namespace AdventOfCode.Solutions.Year2020
                 tile.Bottom, tile.Bottom.Reverse(),
                 tile.Left, tile.Left.Reverse(),
             }.Contains(side);
+
+        bool IsCornerTile(ImageTile tile) => tile.CountMatchingSides() == 2;
+        bool IsEdgeTile(ImageTile tile) => tile.CountMatchingSides() == 3;
 
         void PrintTile(IEnumerable<string> tile) => Console.WriteLine(tile.Select(s => s + "\n").JoinAsStrings());
     }
