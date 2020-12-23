cc2c65b619a5235f4fb82cdbb822a3299b35c568
Sindre S. Kjær <sindre@sindrekjr.no>
2020-12-23 16:08:12 +0100

    Startet Day 23.2

--- AdventOfCode/Solutions/Year2020/Day23/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day23/Solution.cs
@@ -14,12 +14,18 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
+            // var cups = Input.ToIntArray().ToList();
+            // for (int i = cups.Max() + 1; i <= 1000000; i++) cups.Add(i);
+            // cups = ProcessMoves(cups, 10000000);
+            // var target = cups.IndexOf(1);
+            // return cups.Skip(target).Take(2).Aggregate(1, (product, i) => product * i).ToString();
             return null;
         }
 
         List<int> ProcessMoves(List<int> cups, int amount)
         {
             var current = 0;
+            var history = new Dictionary<(int, string), int>();
             for (int c = 0, currentCup = cups[current]; c < amount; c++, currentCup = cups[current])
             {
                 // Console.WriteLine("-- move " + (c + 1) + " --");
@@ -44,6 +50,17 @@ namespace AdventOfCode.Solutions.Year2020
                 // Console.WriteLine("destination: " + destination);
 
                 for (int i = 2, dest = cups.IndexOf(destination) + 1; i >= 0; i--) cups.Insert(dest, picked[i]);
+                
+                var sequence = cups.JoinAsStrings();
+                if (history.ContainsKey((current, sequence)))
+                {
+                    var diff = c - history[(current, sequence)];
+                    c += diff * (int) Math.Floor((double) (amount - c) / diff);
+                }
+                else
+                {
+                    history.Add((current, sequence), c);
+                }
                 
                 current = cups.IndexOf(currentCup) + 1;
                 if (current >= cups.Count) current = 0;
