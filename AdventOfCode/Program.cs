f70fb81552aee8b1bc666474c4b92e5140f85c14
Sindre S. Kjær <sindre@sindrekjr.no>
2021-10-03 16:27:43 +0200

    Added F# project from template

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using AdventOfCode.Infrastructure;
 using AdventOfCode.Infrastructure.Helpers;
-using AdventOfCode.Solutions;
 
 namespace AdventOfCode
 {
     class Program
     {
-        static SolutionCollector Solutions = new SolutionCollector();
+        static readonly SolutionCollector Solutions = new SolutionCollector();
 
         static void Main(string[] args)
         {
-            foreach (ASolution solution in Solutions)
+            foreach (var solution in Solutions)
             {
                 Console.WriteLine();
                 foreach (var line in FormatHelper.FunctionFormat(solution))
---
+++ AdventOfCodeFSharp/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace AdventOfCodeFSharp
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
