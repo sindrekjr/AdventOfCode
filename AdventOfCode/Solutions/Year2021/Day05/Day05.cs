61a6402faef84aa82a891f6e6749dad37e220471
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-06 06:40:18 +0100

    Solved Day 6.1

--- AdventOfCode/Solutions/Year2021/Day05/Day05.cs
+++ AdventOfCode/Solutions/Year2021/Day05/Day05.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace AdventOfCode.Solutions.Year2021
 {
@@ -15,7 +12,7 @@ namespace AdventOfCode.Solutions.Year2021
         protected override string SolvePartTwo() =>
             ParseInput(Input.SplitByNewline(), true).Values.Count(value => value >= 2).ToString();
 
-        Dictionary<(int x, int y), int> ParseInput(string[] lines, bool diagonal = false)
+        Dictionary<(int x, int y), int> ParseInput(IEnumerable<string> lines, bool diagonal = false)
         {
             var map = new Dictionary<(int x, int y), int>();
 
--- AdventOfCode/Solutions/Year2021/Day06/Day06.cs
+++ AdventOfCode/Solutions/Year2021/Day06/Day06.cs
@@ -1,26 +1,66 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2021
 {
-
     class Day06 : ASolution
     {
+        public Day06() : base(06, 2021, "Lanternfish", true) { }
 
-        public Day06() : base(06, 2021, "")
-        {
-
-        }
-
-        protected override string SolvePartOne()
-        {
-            return null;
-        }
+        protected override string SolvePartOne() =>
+            CountFishForDays(Input.ToIntArray(",").ToList(), 80).Count.ToString();
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return SimulateFishForDays(Input.ToIntArray(",").ToList(), 256).ToString();
+        }
+
+        IList<int> CountFishForDays(List<int> fish, int days)
+        {
+            for (var day = 1; day <= days; day++)
+            {
+                var newFish = new List<int>();
+
+                for (var i = 0; i < fish.Count; i++)
+                {
+                    var f = fish[i];
+
+                    if (--f == -1) {
+                        newFish.Add(8);
+                        f = 6;
+                    }
+
+                    fish[i] = f;
+                }
+                
+                fish.AddRange(newFish);
+            }
+
+            return fish;
+        }
+
+        int SimulateFishForDays(IList<int> fish, int days)
+        {
+            var count = 0;
+            
+            for (var i = 0; i <= fish.Count; i += 8)
+            {
+                var f = fish[i];
+                count = FishLife(days - f);
+            }
+
+            return count;
+        }
+
+        int FishLife(int days)
+        {
+            var fishCount = 1;
+
+            for (var reproduction = days - 2; reproduction >= 0; reproduction -= 6)
+            {
+                fishCount += FishLife(reproduction);
+            }
+
+            return fishCount;
         }
     }
 }
