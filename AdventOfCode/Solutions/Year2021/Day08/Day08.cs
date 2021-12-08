1d449e9374442511b56c30ddc856ca0e5bd93e48
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-08 20:46:47 +0100

    Solved Day 8.2

--- AdventOfCode/Solutions/Year2021/Day08/Day08.cs
+++ AdventOfCode/Solutions/Year2021/Day08/Day08.cs
@@ -1,13 +1,8 @@
-using System.Linq;
-
 namespace AdventOfCode.Solutions.Year2021
 {
     class Day08 : ASolution
     {
-        public Day08() : base(08, 2021, "Seven Segment Search")
-        {
-
-        }
+        public Day08() : base(08, 2021, "Seven Segment Search") { }
 
         protected override string SolvePartOne() => Input.SplitByNewline() 
             .Aggregate(0, (acc, entry) => entry
@@ -16,13 +11,61 @@ namespace AdventOfCode.Solutions.Year2021
                 .Aggregate(acc, (a, output) =>
                 {
                     var len = output.Length;
-                    if (len == 2 || len == 4 || len == 3 || len == 7) return a + 1;
+                    if (len is 2 or 4 or 3 or 7) return a + 1;
                     return a;
                 })).ToString();
 
-        protected override string SolvePartTwo()
+        protected override string SolvePartTwo() => Input.SplitByNewline().Aggregate(0, (total, entry) =>
         {
-            return null;
+            var (input, output, _) = entry.Split(" | ");
+            var decodedSignals = DeduceSignals(input.Split(" "));
+
+            var decodedValue = output
+                .Split(" ")
+                .Select(val => val.OrderBy(ch => ch).JoinAsStrings())
+                .Aggregate("", (decodedAcc, value) => decodedAcc + Array.IndexOf(decodedSignals, value));
+
+            return total + int.Parse(decodedValue);
+        }).ToString();
+
+        string[] DeduceSignals(string[] signals)
+        {
+            var one = signals.First(s => s.Length == 2);
+            var four = signals.First(s => s.Length == 4);
+            var seven = signals.First(s => s.Length == 3);
+            var eight = signals.First(s => s.Length == 7);
+
+            var a = seven.First(digit => !one.Contains(digit));
+            var b = four.First(digit => !one.Contains(digit) && signals.Count(s => s.Contains(digit)) == 6);
+            var c = one.First(digit => signals.Count(s => s.Contains(digit)) == 8);
+            var d = four.First(digit => signals.Count(s => s.Contains(digit)) == 7 && digit != b && digit != c);
+            var e = eight.First(digit => signals.Count(s => s.Contains(digit)) == 4);
+            var f = one.First(digit => digit != c);
+            var g = eight.First(digit => digit != e && !one.Contains(digit) && !four.Contains(digit) && !seven.Contains(digit));
+
+            var zero = signals.First(signal => ContainsAll(signal, a, b, c, e, f, g));
+            var two = signals.First(signal => ContainsAll(signal, a, c, d, e, g));
+            var three = signals.First(signal => ContainsAll(signal, a, c, d, f, g));
+            var five = signals.First(signal => ContainsAll(signal, a, b, d, f, g));
+            var six = signals.First(signal => ContainsAll(signal, a, b, d, e, f, g));
+            var nine = signals.First(signal => ContainsAll(signal, a, b, c, d, f, g));
+
+            return new []
+            {
+                zero.OrderBy(ch => ch).JoinAsStrings(),
+                one.OrderBy(ch => ch).JoinAsStrings(),
+                two.OrderBy(ch => ch).JoinAsStrings(),
+                three.OrderBy(ch => ch).JoinAsStrings(),
+                four.OrderBy(ch => ch).JoinAsStrings(),
+                five.OrderBy(ch => ch).JoinAsStrings(),
+                six.OrderBy(ch => ch).JoinAsStrings(),
+                seven.OrderBy(ch => ch).JoinAsStrings(),
+                eight.OrderBy(ch => ch).JoinAsStrings(),
+                nine.OrderBy(ch => ch).JoinAsStrings(),
+            };
         }
+
+        bool ContainsAll(string str, params char[] chars) =>
+            new HashSet<char>(chars).SetEquals(new HashSet<char>(str));
     }
 }
