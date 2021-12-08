bc2f50e7abf3303780a3da45aa4a50174e4bcd8d
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-08 06:18:20 +0100

    Solved Day 8.1

--- AdventOfCode/Solutions/Year2021/Day08/Day08.cs
+++ AdventOfCode/Solutions/Year2021/Day08/Day08.cs
@@ -1,22 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2021
 {
-
     class Day08 : ASolution
     {
-
-        public Day08() : base(08, 2021, "")
+        public Day08() : base(08, 2021, "Seven Segment Search")
         {
 
         }
 
-        protected override string SolvePartOne()
-        {
-            return null;
-        }
+        protected override string SolvePartOne() => Input.SplitByNewline() 
+            .Aggregate(0, (acc, entry) => entry
+                .Split(" | ")[1]
+                .Split(" ")
+                .Aggregate(acc, (a, output) =>
+                {
+                    var len = output.Length;
+                    if (len == 2 || len == 4 || len == 3 || len == 7) return a + 1;
+                    return a;
+                })).ToString();
 
         protected override string SolvePartTwo()
         {
