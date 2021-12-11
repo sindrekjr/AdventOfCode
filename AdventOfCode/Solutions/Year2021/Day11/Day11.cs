172c64bb39152b16870cb57f5630c9c4540cde42
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-11 14:30:38 +0100

    Solved Day 11.2

--- AdventOfCode/Solutions/Year2021/Day11/Day11.cs
+++ AdventOfCode/Solutions/Year2021/Day11/Day11.cs
@@ -3,14 +3,9 @@ using AdventOfCode.Solutions.Year2020;
 
 namespace AdventOfCode.Solutions.Year2021
 {
-
     class Day11 : ASolution
     {
-
-        public Day11() : base(11, 2021, "")
-        {
-
-        }
+        public Day11() : base(11, 2021, "Dumbo Octopus") { }
 
         protected override string SolvePartOne()
         {
@@ -40,13 +35,6 @@ namespace AdventOfCode.Solutions.Year2021
                         octopuses[pos] = 0;
                     }
                 }
-
-                //if (i % 1 == 0)
-                //{
-                //    PaintOctopuses(octopuses);
-                //    Console.WriteLine($"{i}: {flashes} flashes");
-                //    Console.WriteLine();
-                //}
             }
 
             return flashes.ToString();
@@ -54,32 +42,52 @@ namespace AdventOfCode.Solutions.Year2021
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var octopuses = new Map<int>(Input.SplitByNewline().Select(line => line.ToIntArray()).ToArray());
+
+            for (int i = 1;; i++)
+            {
+                var positions = octopuses.Keys.ToArray();
+
+                foreach (var pos in positions) octopuses[pos]++;
+
+                var primed = new HashSet<(int x, int y)>();
+                while (octopuses.Any(o => o.Value > 9 && !primed.Contains(o.Key)))
+                {
+                    var (pos, _) = octopuses.First(o => o.Value > 9 && !primed.Contains(o.Key));
+                    foreach (var adj in FindAdjacentOctopuses(octopuses, pos)) octopuses[adj]++;
+                    primed.Add(pos);
+                }
+
+                foreach (var pos in positions) if (octopuses[pos] > 9) octopuses[pos] = 0;
+
+                if (octopuses.Values.All(energy => energy == 0)) return i.ToString();
+            }
         }
 
         IEnumerable<(int x, int y)> FindAdjacentOctopuses(Map<int> octopuses, (int x, int y) position)
         {
             for (int x = -1; x <= 1; x++) for (int y = -1; y <= 1; y++)
-            {
-                if (x == 0 && y == 0) continue;
-                var (nX, nY) = position.Add((x, y));
-                if (octopuses.ContainsKey((nX, nY))) yield return (nX, nY);
-            }
+                {
+                    if (x == 0 && y == 0) continue;
+                    var (nX, nY) = position.Add((x, y));
+                    if (octopuses.ContainsKey((nX, nY))) yield return (nX, nY);
+                }
         }
 
         void IncreaseAdjacentOctopuses(Map<int> octopuses, HashSet<(int x, int y)> primed, (int x, int y) position)
         {
             primed.Add(position);
             for (int x = -1; x <= 1; x++) for (int y = -1; y <= 1; y++)
-            {
-                if (x == 0 && y == 0) continue;
-                var adjPos = position.Add((x, y));
-                if (octopuses.ContainsKey(adjPos) && !primed.Contains(adjPos)) {
-                    primed.Add(adjPos);
-                    octopuses[adjPos]++;
-                    if (octopuses[adjPos] > 9) IncreaseAdjacentOctopuses(octopuses, primed, adjPos);
+                {
+                    if (x == 0 && y == 0) continue;
+                    var adjPos = position.Add((x, y));
+                    if (octopuses.ContainsKey(adjPos) && !primed.Contains(adjPos))
+                    {
+                        primed.Add(adjPos);
+                        octopuses[adjPos]++;
+                        if (octopuses[adjPos] > 9) IncreaseAdjacentOctopuses(octopuses, primed, adjPos);
+                    }
                 }
-            }
         }
 
         void PaintOctopuses(Map<int> octopuses)
