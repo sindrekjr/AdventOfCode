cbcde73d9625565f5f596e98d51c59edc35aa294
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-11 14:49:01 +0100

    Cleanup Day 11

--- AdventOfCode/Solutions/Year2021/Day11/Day11.cs
+++ AdventOfCode/Solutions/Year2021/Day11/Day11.cs
@@ -10,99 +10,48 @@ namespace AdventOfCode.Solutions.Year2021
         protected override string SolvePartOne()
         {
             var octopuses = new Map<int>(Input.SplitByNewline().Select(line => line.ToIntArray()).ToArray());
-
-            var flashes = 0;
-            for (int i = 1; i <= 100; i++)
-            {
-                var positions = octopuses.Keys.ToArray();
-
-                foreach (var pos in positions) octopuses[pos]++;
-
-                var primed = new HashSet<(int x, int y)>();
-                while (octopuses.Any(o => o.Value > 9 && !primed.Contains(o.Key)))
-                {
-                    var (pos, _) = octopuses.First(o => o.Value > 9 && !primed.Contains(o.Key));
-                    foreach (var adj in FindAdjacentOctopuses(octopuses, pos)) octopuses[adj]++;
-                    primed.Add(pos);
-                }
-
-                foreach (var pos in positions)
-                {
-                    var energy = octopuses[pos];
-                    if (energy > 9)
-                    {
-                        flashes++;
-                        octopuses[pos] = 0;
-                    }
-                }
-            }
-
-            return flashes.ToString();
+            return Enumerable.Range(0, 100).Aggregate(0, (flashes, _) => flashes + FlashStep(octopuses)).ToString();
         }
 
         protected override string SolvePartTwo()
         {
             var octopuses = new Map<int>(Input.SplitByNewline().Select(line => line.ToIntArray()).ToArray());
+            for (int i = 1;; i++) if (FlashStep(octopuses) == octopuses.Count) return i.ToString();
+        }
 
-            for (int i = 1;; i++)
+        int FlashStep(Map<int> octopuses)
+        {
+            var primed = new HashSet<(int, int)>();
+            var positions = octopuses.Keys.ToArray();
+
+            foreach (var pos in positions) octopuses[pos]++;
+
+            while (octopuses.Any(o => o.Value > 9 && !primed.Contains(o.Key)))
             {
-                var positions = octopuses.Keys.ToArray();
+                var (pos, _) = octopuses.First(o => o.Value > 9 && !primed.Contains(o.Key));
+                foreach (var adj in FindAdjacentOctopuses(octopuses, pos)) octopuses[adj]++;
+                primed.Add(pos);
+            }
 
-                foreach (var pos in positions) octopuses[pos]++;
-
-                var primed = new HashSet<(int x, int y)>();
-                while (octopuses.Any(o => o.Value > 9 && !primed.Contains(o.Key)))
+            return positions.Count(pos =>
+            {
+                if (octopuses[pos] > 9)
                 {
-                    var (pos, _) = octopuses.First(o => o.Value > 9 && !primed.Contains(o.Key));
-                    foreach (var adj in FindAdjacentOctopuses(octopuses, pos)) octopuses[adj]++;
-                    primed.Add(pos);
+                    octopuses[pos] = 0;
+                    return true;
                 }
 
-                foreach (var pos in positions) if (octopuses[pos] > 9) octopuses[pos] = 0;
-
-                if (octopuses.Values.All(energy => energy == 0)) return i.ToString();
-            }
+                return false;
+            });
         }
 
         IEnumerable<(int x, int y)> FindAdjacentOctopuses(Map<int> octopuses, (int x, int y) position)
         {
             for (int x = -1; x <= 1; x++) for (int y = -1; y <= 1; y++)
-                {
-                    if (x == 0 && y == 0) continue;
-                    var (nX, nY) = position.Add((x, y));
-                    if (octopuses.ContainsKey((nX, nY))) yield return (nX, nY);
-                }
-        }
-
-        void IncreaseAdjacentOctopuses(Map<int> octopuses, HashSet<(int x, int y)> primed, (int x, int y) position)
-        {
-            primed.Add(position);
-            for (int x = -1; x <= 1; x++) for (int y = -1; y <= 1; y++)
-                {
-                    if (x == 0 && y == 0) continue;
-                    var adjPos = position.Add((x, y));
-                    if (octopuses.ContainsKey(adjPos) && !primed.Contains(adjPos))
-                    {
-                        primed.Add(adjPos);
-                        octopuses[adjPos]++;
-                        if (octopuses[adjPos] > 9) IncreaseAdjacentOctopuses(octopuses, primed, adjPos);
-                    }
-                }
-        }
-
-        void PaintOctopuses(Map<int> octopuses)
-        {
-            for (int x = 0; x < 10; x++)
             {
-                for (int y = 0; y < 10; y++)
-                {
-                    var energy = octopuses[(x, y)];
-                    if (energy == 0) Console.BackgroundColor = ConsoleColor.White;
-                    Console.Write(energy);
-                    Console.ResetColor();
-                }
-
-                Console.WriteLine();
+                if (x == 0 && y == 0) continue;
+                var (aX, aY) = position.Add((x, y));
+                if (octopuses.ContainsKey((aX, aY))) yield return (aX, aY);
             }
         }
     }
