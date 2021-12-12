bdd904dbd835f171503dcdfb8560ad7b177b00a7
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-12 12:00:05 +0100

    Solved Day 12.2

--- AdventOfCode/Solutions/Year2021/Day12/Day12.cs
+++ AdventOfCode/Solutions/Year2021/Day12/Day12.cs
@@ -12,22 +12,22 @@ namespace AdventOfCode.Solutions.Year2021
         protected override string SolvePartOne() =>
             Connections["end"].Aggregate(0, (paths, p) => paths + CountPaths(p, "end")).ToString();
 
-        protected override string SolvePartTwo()
-        {
-            return null;
-        }
+        protected override string SolvePartTwo() =>
+            Connections["end"].Aggregate(0, (paths, p) => paths + CountPaths(p, "end", true)).ToString();
 
-        int CountPaths(string cave, string path)
+        int CountPaths(string cave, string path, bool canVisitSmallTwice = false)
         {
+            if (cave.ToLower() == cave && canVisitSmallTwice) canVisitSmallTwice = !path.Contains(cave);
             path += $"-{cave}";
 
             var count = 0;
             foreach (var p in Connections[cave])
             {
-                if (path.Contains(p) && p.ToLower() == p) continue;
+                if (p == "end" || path.Contains(p) && p.ToLower() == p && !canVisitSmallTwice) continue;
+
                 count += p == "start"
                     ? 1
-                    : CountPaths(p, path);
+                    : CountPaths(p, path, canVisitSmallTwice);
             }
 
             return count;
