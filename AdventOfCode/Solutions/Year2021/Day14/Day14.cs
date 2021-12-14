28f5a739bdd17ccaaa0559d560586f3129eeffdd
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-14 07:13:40 +0100

    Solved Day 14.1

--- AdventOfCode/Solutions/Year2021/Day14/Day14.cs
+++ AdventOfCode/Solutions/Year2021/Day14/Day14.cs
@@ -4,23 +4,76 @@ using System.Text;
 
 namespace AdventOfCode.Solutions.Year2021
 {
-
     class Day14 : ASolution
     {
-
-        public Day14() : base(14, 2021, "")
-        {
-
-        }
+        public Day14() : base(14, 2021, "Extended Polymerization") { }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var (template, rules) = ParseInputAsQueue();
+
+            for (int i = 0; i < 10; i++) template = Step(template, rules);
+
+            var counter = new Dictionary<char, int>();
+            foreach (var ch in template)
+            {
+                if (!counter.ContainsKey(ch)) counter.Add(ch, 0);
+                counter[ch]++;
+            }
+
+            return (counter.Values.Max() - counter.Values.Min()).ToString();
         }
 
         protected override string SolvePartTwo()
         {
+            // var (template, rules) = ParseInputAsQueue();
+
+            // for (int i = 0; i < 40; i++) template = Step(template, rules);
+
+            // var counter = new Dictionary<char, long>();
+            // foreach (var ch in template)
+            // {
+            //     if (!counter.ContainsKey(ch)) counter.Add(ch, 0);
+            //     counter[ch]++;
+            // }
+
+            // return (counter.Values.Max() - counter.Values.Min()).ToString();
+
             return null;
+        }
+
+        Queue<char> Step(Queue<char> template, Dictionary<string, char> rules)
+        {
+            var newTemplate = new Queue<char>();
+            while (template.Count != 0)
+            {
+                var current = template.Dequeue();
+                newTemplate.Enqueue(current);
+
+                if (template.TryPeek(out char next))
+                {
+                    newTemplate.Enqueue(rules[$"{current}{next}"]);
+                }
+            }
+
+            return newTemplate;
+        }
+
+        (Queue<char>, Dictionary<string, char>) ParseInputAsQueue()
+        {
+            var (template, rules, _) = Input.SplitByParagraph();
+
+            var queue = new Queue<char>();
+            foreach (var ch in template) queue.Enqueue(ch);
+
+            var dict = new Dictionary<string, char>();
+            foreach (var rule in rules.SplitByNewline())
+            {
+                var (couple, result, _) = rule.Split(" -> ");
+                dict.Add(couple, result.ToCharArray().First());
+            }
+
+            return (queue, dict);
         }
     }
 }
