eeae3d2597d0fe35e2438b30d84ed74f3a4ce4f7
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-14 21:34:22 +0100

    Cleanup Day 14

--- AdventOfCode/Solutions/Year2021/Day14/Day14.cs
+++ AdventOfCode/Solutions/Year2021/Day14/Day14.cs
@@ -6,35 +6,10 @@ namespace AdventOfCode.Solutions.Year2021
 
         protected override string SolvePartOne()
         {
-            var (template, rules) = ParseInput();
+            var (pairs, rules) = ParseInput();
+            var last = pairs.Last().Key.Last();
 
-            for (int i = 0; i < 10; i++) template = Step(template.JoinAsStrings(), rules);
-
-            var counter = new Dictionary<char, int>();
-            foreach (var ch in template)
-            {
-                if (!counter.ContainsKey(ch)) counter.Add(ch, 0);
-                counter[ch]++;
-            }
-
-            return (counter.Values.Max() - counter.Values.Min()).ToString();
-        }
-
-        protected override string SolvePartTwo()
-        {
-            var (template, rules) = ParseInput();
-
-            var pairs = new Dictionary<string, long>();
-            for (int i = 0; i < template.Length - 1; i++)
-            {
-                var pair = $"{template[i]}{template[i + 1]}";
-                if (!pairs.ContainsKey(pair)) pairs.Add(pair, 0);
-                pairs[pair] += 1;
-            }
-
-            for (int i = 0; i < 40; i++) pairs = NewStep(pairs, rules);
-
-            var counter = pairs.Aggregate(new Dictionary<char, long>(), (dict, pair) =>
+            var counter = Step(pairs, rules, 10).Aggregate(new Dictionary<char, long>(), (dict, pair) =>
             {
                 var (ch, _) = pair.Key.ToArray();
                 if (!dict.ContainsKey(ch)) dict.Add(ch, 0);
@@ -42,12 +17,33 @@ namespace AdventOfCode.Solutions.Year2021
                 return dict;
             });
 
-            counter[template.Last()] += 1;
+            counter[last] += 1;
 
             return (counter.Values.Max() - counter.Values.Min()).ToString();
         }
 
-        Dictionary<string, long> NewStep(Dictionary<string, long> pairs, Dictionary<string, char> rules)
+        protected override string SolvePartTwo()
+        {
+            var (pairs, rules) = ParseInput();
+            var last = pairs.Last().Key.Last();
+
+            var counter = Step(pairs, rules, 40).Aggregate(new Dictionary<char, long>(), (dict, pair) =>
+            {
+                var (ch, _) = pair.Key.ToArray();
+                if (!dict.ContainsKey(ch)) dict.Add(ch, 0);
+                dict[ch] += pair.Value;
+                return dict;
+            });
+
+            counter[last] += 1;
+
+            return (counter.Values.Max() - counter.Values.Min()).ToString();
+        }
+
+        Dictionary<string, long> Step(Dictionary<string, long> pairs, Dictionary<string, char> rules, int steps) =>
+            Enumerable.Range(0, steps).Aggregate(pairs, (pairs, _) => Step(pairs, rules));
+
+        Dictionary<string, long> Step(Dictionary<string, long> pairs, Dictionary<string, char> rules)
         {
             var newPairs = new Dictionary<string, long>();
 
@@ -66,41 +62,26 @@ namespace AdventOfCode.Solutions.Year2021
         string[] GetResultingPairs(string pair, Dictionary<string, char> rules) =>
             new[] { $"{pair[0]}{rules[pair]}", $"{rules[pair]}{pair[1]}" };
 
-        string Step(string template, Dictionary<string, char> rules)
-        {
-            var newTemplate = "";
-
-            for (int i = 0; i < template.Length; i++)
-            {
-                var current = template[i];
-                newTemplate += current;
-
-                if (i + 1 < template.Length)
-                {
-                    if (rules.TryGetValue($"{current}{template[i + 1]}", out char value)) newTemplate += value;
-                }
-            }
-
-            return newTemplate;
-        }
-
-        (string, Dictionary<string, char>) ParseInput()
+        (Dictionary<string, long>, Dictionary<string, char>) ParseInput()
         {
             var (template, rules, _) = Input.SplitByParagraph();
 
-            var dict = new Dictionary<string, char>();
+            var pairs = new Dictionary<string, long>();
+            for (int i = 0; i < template.Length - 1; i++)
+            {
+                var pair = $"{template[i]}{template[i + 1]}";
+                if (!pairs.ContainsKey(pair)) pairs.Add(pair, 0);
+                pairs[pair] += 1;
+            }
+
+            var rulesDict = new Dictionary<string, char>();
             foreach (var rule in rules.SplitByNewline())
             {
                 var (couple, result, _) = rule.Split(" -> ");
-                dict.Add(couple, result.ToCharArray().First());
+                rulesDict.Add(couple, result.ToCharArray().First());
             }
 
-            return (template, dict);
+            return (pairs, rulesDict);
         }
-    }
-
-    internal class StringLengthComparer : IComparer<string>
-    {
-        public int Compare(string x, string y) => y.Length - x.Length;
     }
 }
