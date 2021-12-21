51384a0a1a700e00a9d95ffed4d6edb936b31b3b
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-21 10:09:31 +0100

    Solved Day 21.1?

--- AdventOfCode/Solutions/Year2021/Day21/Day21.cs
+++ AdventOfCode/Solutions/Year2021/Day21/Day21.cs
@@ -1,26 +1,48 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace AdventOfCode.Solutions.Year2021
 {
-
     class Day21 : ASolution
     {
-
-        public Day21() : base(21, 2021, "")
-        {
-
-        }
+        public Day21() : base(21, 2021, "Dirac Dice") { }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var (player1, player2, _) = Input.SplitByNewline();
+            var a = int.Parse(player1.Split(":").Last().Trim());
+            var b = int.Parse(player2.Split(":").Last().Trim());
+            var (rolls, scores) = PlayDeterministic(a, b);
+            return (--rolls * scores.Min()).ToString();
         }
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        (int rolls, int[] scores) PlayDeterministic(int aPos, int bPos)
+        {
+            var deterministic = 1;
+            var aScore = 0;
+            var bScore = 0;
+            var rolls = 0;
+
+            while (aScore <= 1000 && bScore <= 1000)
+            {
+                rolls += 3;
+                var roll = deterministic++ + deterministic++ + deterministic++;
+                aPos = (aPos + roll) % 10;
+                if (aPos == 0) aPos = 10;
+                aScore += aPos;
+
+                if (aScore >= 1000) break;
+
+                rolls += 3;
+                roll = deterministic++ + deterministic++ + deterministic++;
+                bPos = (bPos + roll) % 10;
+                if (bPos == 0) bPos = 10;
+                bScore += bPos;
+            }
+
+            return (deterministic, new[] { aScore, bScore });
         }
     }
 }
