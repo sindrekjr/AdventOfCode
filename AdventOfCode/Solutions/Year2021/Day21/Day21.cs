ef7c2636e25bfdccc79a76851555fc13081f765c
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-23 00:22:40 +0100

    Started on Day 21.2

--- AdventOfCode/Solutions/Year2021/Day21/Day21.cs
+++ AdventOfCode/Solutions/Year2021/Day21/Day21.cs
@@ -2,7 +2,9 @@ namespace AdventOfCode.Solutions.Year2021
 {
     class Day21 : ASolution
     {
-        public Day21() : base(21, 2021, "Dirac Dice") { }
+        Dictionary<GameState, (long a, long b)> Memo = new Dictionary<GameState, (long, long)>();
+
+        public Day21() : base(21, 2021, "Dirac Dice", true) { }
 
         protected override string SolvePartOne()
         {
@@ -15,6 +17,14 @@ namespace AdventOfCode.Solutions.Year2021
 
         protected override string SolvePartTwo()
         {
+            var (player1, player2, _) = Input.SplitByNewline();
+            var a = int.Parse(player1.Split(":").Last().Trim());
+            var b = int.Parse(player2.Split(":").Last().Trim());
+            var results = PlayQuantum(new PlayerState(a, 0), new PlayerState(b, 0));
+
+            Console.WriteLine(results.a);
+            Console.WriteLine(results.b);
+
             return null;
         }
 
@@ -44,5 +54,63 @@ namespace AdventOfCode.Solutions.Year2021
 
             return (deterministic, new[] { aScore, bScore });
         }
+
+        (long a, long b) PlayQuantum(PlayerState a, PlayerState b)
+        {
+            (long a, long b) wins = (0, 0);
+
+            foreach (var roll in GetPossibleRolls())
+            {
+                var state = new GameState(a, b, 0, roll);
+                if (Memo.ContainsKey(state))
+                {
+                    var result = Memo[state];
+                    wins = (wins.a + result.a, wins.b + result.b);
+                    continue;
+                }
+
+                var aNext = a.NextState(roll);
+                if (aNext.Score >= 21)
+                {
+                    wins.a++;
+                    continue;
+                }
+
+                var bNext = b.NextState(roll);
+                if (bNext.Score >= 21)
+                {
+                    wins.b++;
+                    continue;
+                }
+
+                var subGameResult = PlayQuantum(aNext, bNext);
+                wins.a += subGameResult.a;
+                wins.b += subGameResult.b;
+
+                Memo[state] = subGameResult;
+            }
+
+            return wins;
+        }
+
+        IEnumerable<int> GetPossibleRolls()
+        {
+            for (int i = 1; i <= 3; i++) for (int j = 1; j <= 3; j++) for (int k = 1; k <= 3; k++)
+            {
+                yield return i + j + k;
+            }
+        }
     }
+
+    internal record PlayerState(int Position, int Score)
+    {
+        public PlayerState NextState(int roll)
+        {
+            var nextPosition = (Position + roll - 1) % 10 + 1;
+            var nextScore = Score + nextPosition;
+            return new PlayerState(nextPosition, nextScore);
+        }
+    }
+
+    internal record GameState(PlayerState Player1, PlayerState Player2, int Turn, int Roll) { }
 }
