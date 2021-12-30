93736552ab9753a4572a33a2050e6c3dfac5fab8
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-30 01:51:25 +0100

    Add support for supplying day to run through dotnet run

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿using AdventOfCode.Solutions;
+﻿using AdventOfCode.Services;
+using AdventOfCode.Solutions;
+
+var year = ConfigurationService.GetYear();
+var days = ConfigurationService.GetDays();
+
+if (args.Length > 0 && int.TryParse(args.First(), out int day)) days = new[] { day };
 
 Console.WriteLine();
-foreach (var solution in SolutionCollector.FetchSolutions())
+foreach (var solution in SolutionCollector.FetchSolutions(year, days))
 {
     Console.WriteLine(solution.ToString());
 }
