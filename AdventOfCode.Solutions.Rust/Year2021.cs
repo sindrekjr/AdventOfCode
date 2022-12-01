05ae9a95d7ff26b148289ee2e21956091c299006
Sindre S. Kjær <sindre@sindrekjr.no>
2022-12-01 18:18:52 +0100

    Successfully call Rust lib with integers

--- AdventOfCode.Solutions.Rust/Year2021.cs
+++ AdventOfCode.Solutions.Rust/Year2021.cs
@@ -4,11 +4,7 @@ namespace AdventOfCode.Solutions.Rust;
 
 public static class Year2021
 {
-    [DllImport("solutions2021.dll", EntryPoint = "solve")]
-    private static extern void Solve();
+    [DllImport("solutions2021.dll")] private static extern void solve(int year, int day, int part);
 
-    public static void Test()
-    {
-        Solve();
-    }
+    public static void Solve(int day, int part) => solve(2021, day, part);
 }
--- AdventOfCode.Solutions.Rust/src/lib.rs
+++ AdventOfCode.Solutions.Rust/src/lib.rs
@@ -1,4 +1,4 @@
 #[no_mangle]
-pub extern fn solve() {
-    println!("Hello world");
+pub extern fn solve(year: i32, day: i32, part: i32) {
+    println!("{}", format!("Hello C#! Solving Day {} Part {}, from AoC{}", day, part, year));
 }
