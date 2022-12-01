366380a965c43307b136ca9574fe7e652aaf9470
Sindre S. Kjær <sindre@sindrekjr.no>
2022-12-01 06:12:55 +0100

    Cleanup Day 1

--- AdventOfCode.Solutions/Year2022/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day01/Solution.cs
@@ -2,17 +2,19 @@ namespace AdventOfCode.Solutions.Year2022.Day01;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(01, 2022, "") { }
+    public Solution() : base(01, 2022, "Calorie Counting") { }
 
-    protected override string SolvePartOne()
-    {
-        var input = Input.SplitByParagraph().Select(p => p.SplitByNewline().Select(int.Parse).Sum());
-        return input.Max().ToString();
-    }
+    protected override string SolvePartOne() => Input
+        .SplitByParagraph()
+        .Select(elf => elf.SplitByNewline().Select(int.Parse).Sum())
+        .Max()
+        .ToString();
 
-    protected override string SolvePartTwo()
-    {
-        var input = Input.SplitByParagraph().Select(p => p.SplitByNewline().Select(int.Parse).Sum());
-        return input.OrderByDescending(elf => elf).Take(3).Sum().ToString();
-    }
+    protected override string SolvePartTwo() => Input
+        .SplitByParagraph()
+        .Select(p => p.SplitByNewline().Select(int.Parse).Sum())
+        .OrderByDescending(elf => elf)
+        .Take(3)
+        .Sum()
+        .ToString();
 }
