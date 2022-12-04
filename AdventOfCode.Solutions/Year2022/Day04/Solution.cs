3dc18dc54b4e6844152d10ac43223c6ee7464d76
Sindre S. Kjær <sindre@sindrekjr.no>
2022-12-04 10:53:58 +0100

    Solved Day 4

--- AdventOfCode.Solutions/Year2022/Day04/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day04/Solution.cs
@@ -2,15 +2,29 @@ namespace AdventOfCode.Solutions.Year2022.Day04;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(04, 2022, "") { }
+    public Solution() : base(04, 2022, "Camp Cleanup") { }
 
-    protected override string SolvePartOne()
-    {
-        return "";
-    }
+    protected override string SolvePartOne() => Input.SplitByNewline()
+        .Select(pair =>
+        {
+            var (a, b, _) = pair.Split(",");
 
-    protected override string SolvePartTwo()
-    {
-        return "";
-    }
+            var (sA, eA, _) = a.Split("-").Select(int.Parse).ToArray();
+            var (sB, eB, _) = b.Split("-").Select(int.Parse).ToArray();
+
+            return (sA >= sB && eA <= eB) || (sA <= sB && eA >= eB);
+        })
+        .Count(contained => contained).ToString();
+
+    protected override string SolvePartTwo() => Input.SplitByNewline()
+        .Select(pair =>
+        {
+            var (a, b, _) = pair.Split(",");
+
+            var (sA, eA, _) = a.Split("-").Select(int.Parse).ToArray();
+            var (sB, eB, _) = b.Split("-").Select(int.Parse).ToArray();
+
+            return (sA >= sB && eB >= sA) || (sA <= sB && eA >= sB);
+        })
+        .Count(overlap => overlap).ToString();
 }
