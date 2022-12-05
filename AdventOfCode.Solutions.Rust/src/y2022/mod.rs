c866b45179d455b1d70bbc2e00473069a12a028a
Sindre S. Kjær <sindre@sindrekjr.no>
2022-12-05 08:23:01 +0100

    Solved Day 5

--- AdventOfCode.Solutions.Rust/src/y2022/mod.rs
+++ AdventOfCode.Solutions.Rust/src/y2022/mod.rs
@@ -1,8 +1,10 @@
 mod d01;
+mod d05;
 
 pub fn get_solution(day: i32, part: i32, input: String) -> String {
     match day {
         1 => d01::solve(part, input),
+        5 => d05::solve(part, input),
         _ => String::new()
     }
 }
