636ac2ea53f64cbc78e61da001003ef125853868
Sindre S. Kjær <sindre@sindrekjr.no>
2022-12-09 22:55:45 +0100

    Solved Day 9 Part 2

--- AdventOfCode.Solutions.Rust/src/y2022/d09.rs
+++ AdventOfCode.Solutions.Rust/src/y2022/d09.rs
@@ -9,66 +9,101 @@ pub fn solve(part: Part, input: String) -> String {
     }
 }
 
+struct Trail {
+    knots: Vec<(i32, i32)>,
+    length: usize,
+}
+
+impl Trail {
+    fn new(length: usize) -> Self {
+        Self {
+            knots: vec![(0, 0); length],
+            length,
+        }
+    }
+
+    fn do_moves<I>(&mut self, moves: I) -> HashSet<(i32, i32)>
+    where
+        I: Iterator<Item = (char, usize)>,
+    {
+        moves
+            .flat_map(|(mv, steps)| {
+                (0..steps)
+                    .map(|_| {
+                        match mv {
+                            'R' => self.knots[0].0 += 1,
+                            'L' => self.knots[0].0 -= 1,
+                            'U' => self.knots[0].1 += 1,
+                            'D' => self.knots[0].1 -= 1,
+                            _ => panic!(),
+                        }
+
+                        self.catch_up();
+                        self.knots[self.length - 1]
+                    })
+                    .collect::<Vec<(i32, i32)>>()
+            })
+            .collect()
+    }
+
+    fn catch_up(&mut self) {
+        for i in 1..self.length {
+            let head = self.knots[i - 1];
+            let slack = self.knots[i];
+
+            if !adjacent(&head, &slack) && !same(&head, &slack) {
+                self.knots[i] = *self
+                    .peek_around(head)
+                    .iter()
+                    .find(|adj| adjacent(adj, &slack))
+                    .unwrap();
+            }
+        }
+    }
+
+    fn peek_around(&self, (x, y): (i32, i32)) -> [(i32, i32); 8] {
+        [
+            (x, y - 1),
+            (x, y + 1),
+            (x - 1, y),
+            (x + 1, y),
+            (x + 1, y + 1),
+            (x - 1, y - 1),
+            (x + 1, y - 1),
+            (x - 1, y + 1),
+        ]
+    }
+}
+
 struct Day08;
 impl Solution for Day08 {
     fn solve_part_one(input: String) -> String {
-        let mut head = vec![(0, 0)];
-        let mut tail = vec![(0, 0)];
-
-        input.lines().for_each(|mv| {
-            let (direction, steps) = mv.split_once(' ').unwrap();
-            
-            (0..steps.parse::<usize>().unwrap()).for_each(|_| {
-                let prev = head.last().unwrap().to_owned();
-                let next = match direction {
-                    "R" => (prev.0 + 1, prev.1),
-                    "L" => (prev.0 - 1, prev.1),
-                    "U" => (prev.0, prev.1 + 1),
-                    "D" => (prev.0, prev.1 - 1),
-                    _ => panic!()
-                };
-
-                head.push(next);
-
-                if !adjacent(tail.last().unwrap(), &next) {
-                    tail.push(prev);
-                }
-            })
-        });
-
-        tail.into_iter().collect::<HashSet<(i32, i32)>>().len().to_string()
+        Trail::new(2)
+            .do_moves(input.lines().map(parse_move))
+            .len()
+            .to_string()
     }
 
     fn solve_part_two(input: String) -> String {
-        let mut head = vec![(0, 0); 10];
-        let mut tail = vec![(0, 0)];
-
-        input.lines().for_each(|mv| {
-            let (direction, steps) = mv.split_once(' ').unwrap();
-            
-            (0..steps.parse::<usize>().unwrap()).for_each(|_| {
-                let prev = head[0];
-                let next = match direction {
-                    "R" => (prev.0 + 1, prev.1),
-                    "L" => (prev.0 - 1, prev.1),
-                    "U" => (prev.0, prev.1 + 1),
-                    "D" => (prev.0, prev.1 - 1),
-                    _ => panic!()
-                };
-
-                head.insert(0, next);
-                let end = head.pop().unwrap();
-
-                if !adjacent(tail.last().unwrap(), &end) {
-                    tail.push(end);
-                }
-            })
-        });
-
-        tail.into_iter().collect::<HashSet<(i32, i32)>>().len().to_string()
+        Trail::new(10)
+            .do_moves(input.lines().map(parse_move))
+            .len()
+            .to_string()
     }
+}
+
+fn parse_move(mv: &str) -> (char, usize) {
+    let (direction, steps) = mv.split_once(' ').unwrap();
+    return (
+        direction.chars().collect::<Vec<char>>()[0],
+        steps.parse().unwrap(),
+    );
 }
 
 fn adjacent(a: &(i32, i32), b: &(i32, i32)) -> bool {
     (a.0 - b.0).abs() <= 1 && (a.1 - b.1).abs() <= 1
+}
+
+fn same(a: &(i32, i32), b: &(i32, i32)) -> bool {
+    a.0 == b.0 && a.1 == b.1
 }
