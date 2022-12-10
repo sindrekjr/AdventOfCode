88f4f60d759fe757580ecb065bf518ca10b04f14
Sindre S. Kjær <sindre@sindrekjr.no>
2022-12-10 09:41:46 +0100

    Cleanup Day 10

--- AdventOfCode.Solutions.Rust/src/y2022/d10.rs
+++ AdventOfCode.Solutions.Rust/src/y2022/d10.rs
@@ -1,3 +1,5 @@
+use std::str::Lines;
+
 use crate::core::{Part, Solution};
 
 pub fn solve(part: crate::core::Part, input: String) -> String {
@@ -10,48 +12,44 @@ pub fn solve(part: crate::core::Part, input: String) -> String {
 struct Day10;
 impl Solution for Day10 {
     fn solve_part_one(input: String) -> String {
-        let mut signals = vec![1];
-
-        for line in input.lines().map(|l| l.split_once(' ')) {
-            if let Some((_, val)) = line {
-                signals.push(*signals.last().unwrap());
-                signals.push(*signals.last().unwrap() + val.parse::<i32>().unwrap());
-            } else {
-                signals.push(*signals.last().unwrap());
-            }
-        }
+        let cycles = parse_cycles(input.lines());
 
         [20, 60, 100, 140, 180, 220]
             .iter()
-            .map(|cycle| signals[cycle - 1] as usize * cycle)
+            .map(|cycle| cycles[cycle - 1] as usize * cycle)
             .sum::<usize>()
             .to_string()
     }
 
     fn solve_part_two(input: String) -> String {
-        let mut cycles = vec![1];
+        parse_cycles(input.lines())
+            .iter()
+            .enumerate()
+            .map(|(c, x)| {
+                let horizontal = (c % 40) as i32;
+                let del = if horizontal == 0 { "\n" } else { "" };
 
-        for line in input.lines().map(|l| l.split_once(' ')) {
-            if let Some((_, val)) = line {
-                cycles.push(*cycles.last().unwrap());
-                cycles.push(*cycles.last().unwrap() + val.parse::<i32>().unwrap());
-            } else {
-                cycles.push(*cycles.last().unwrap());
-            }
-        }
-
-        cycles.iter().enumerate().map(|(c, x)| {
-            let del = if c % 40 == 0 {
-                "\n"
-            } else {
-                ""
-            };
-
-            if (x - (c % 40) as i32).abs() <= 1 {
-                format!("{}#", del)
-            } else {
-                format!("{}.", del)
-            }
-        }).collect()
+                let x_distance = x - horizontal;
+                if x_distance <= 1 && x_distance >= -1 {
+                    format!("{}#", del)
+                } else {
+                    format!("{}.", del)
+                }
+            })
+            .collect()
     }
+}
+
+fn parse_cycles(lines: Lines<'_>) -> Vec<i32> {
+    let mut cycles = vec![1];
+    for line in lines {
+        if let Some((_, val)) = line.split_once(' ') {
+            cycles.push(*cycles.last().unwrap());
+            cycles.push(*cycles.last().unwrap() + val.parse::<i32>().unwrap());
+        } else {
+            cycles.push(*cycles.last().unwrap());
+        }
+    }
+
+    cycles
 }
