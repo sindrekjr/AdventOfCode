26e0c39c00ab758da1d86bec88f9444a6166ac6e
Sindre S. Kjær <sindre@sindrekjr.no>
2022-12-15 14:15:24 +0100

    Brute-force Day 15 Part 2?

--- AdventOfCode.Solutions.Rust/src/y2022/d15.rs
+++ AdventOfCode.Solutions.Rust/src/y2022/d15.rs
@@ -80,9 +80,9 @@ impl Solution for Day15 {
         (x_min..x_max)
             .fold(0, |acc, x| {
                 let pos = &Position { x, y: Y };
-                let covered = sensors.values().any(|sensor| sensor.within_coverage(pos) && pos != &sensor.closest_beacon);
-
-                // println!("{:?} = {}", pos, covered);
+                let covered = sensors
+                    .values()
+                    .any(|sensor| sensor.within_coverage(pos) && pos != &sensor.closest_beacon);
 
                 if covered {
                     acc + 1
@@ -93,7 +93,66 @@ impl Solution for Day15 {
             .to_string()
     }
 
-    fn solve_part_two(_input: String) -> String {
-        String::new()
+    fn solve_part_two(input: String) -> String {
+        const MIN: isize = 0;
+        const MAX: isize = 4_000_000;
+        // const MAX: isize = 20;
+        let mut x_min = MAX;
+        let mut x_max = MIN;
+        let mut y_min = MAX;
+        let mut y_max = MIN;
+
+        let sensors: HashMap<Position<isize>, Sensor> = input
+            .lines()
+            .map(|line| {
+                let sensor = Sensor::from(line);
+                let beacon_distance = sensor.beacon_distance();
+
+                let x = sensor.position.x - beacon_distance;
+                if x >= MIN && x < x_min {
+                    x_min = x;
+                }
+
+                let x = sensor.position.x + beacon_distance;
+                if x <= MAX && x > x_max {
+                    x_max = x;
+                }
+
+                let y = sensor.position.y - beacon_distance;
+                if y >= MIN && y < y_min {
+                    y_min = y;
+                }
+
+                let y = sensor.position.y + beacon_distance;
+                if y <= MAX && y > y_max {
+                    y_max = y;
+                }
+
+                (sensor.position, sensor)
+            })
+            .collect();
+
+        let distress_beacon = (x_min..x_max)
+            .find_map(|x| {
+                (y_min..y_max).find_map(|y| {
+                    let pos = Position { x, y };
+
+                    if sensors
+                        .values()
+                        .any(|sensor| sensor.within_coverage(&pos))
+                    {
+                        None
+                    } else {
+                        Some(pos)
+                    }
+                })
+            })
+            .unwrap();
+
+        (distress_beacon.x * 4_000_000 + distress_beacon.y).to_string()
+
+        // println!("x: {} - {}; y {} - {}", x_min, x_max, y_min, y_max);
+
+        // String::new()
     }
 }
