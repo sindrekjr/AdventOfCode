2730a6cffe017e678e71202bfc3f8a141d5f87e5
Sindre S. Kjær <sindre@sindrekjr.no>
2022-12-19 20:53:32 +0100

    Store Day 19

--- AdventOfCode.Solutions.Rust/src/y2022/d19.rs
+++ AdventOfCode.Solutions.Rust/src/y2022/d19.rs
@@ -7,13 +7,85 @@ pub fn solve(part: Part, input: String) -> String {
     }
 }
 
+#[derive(Debug)]
+struct Blueprint {
+    id: u8,
+    ore: u8,
+    clay: u8,
+    obsidian: (u8, u8),
+    geode: (u8, u8),
+}
+
+impl From<&str> for Blueprint {
+    fn from(inp: &str) -> Self {
+        let mut split = inp.split("Each ").map(|design| design.split_whitespace());
+
+        let id = split
+            .next()
+            .unwrap()
+            .nth(1)
+            .unwrap()
+            .split(':')
+            .next()
+            .unwrap()
+            .parse::<u8>()
+            .unwrap();
+
+        let ore = split.next().unwrap().nth(3).unwrap().parse::<u8>().unwrap();
+        let clay = split.next().unwrap().nth(3).unwrap().parse::<u8>().unwrap();
+
+        let obsidian = match split
+            .next()
+            .unwrap()
+            .skip(3)
+            .collect::<Vec<&str>>()
+            .as_slice()
+        {
+            &[a, _, _, b, ..] => (a.parse::<u8>().unwrap(), b.parse::<u8>().unwrap()),
+            _ => panic!(),
+        };
+
+        let geode = match split
+            .next()
+            .unwrap()
+            .skip(3)
+            .collect::<Vec<&str>>()
+            .as_slice()
+        {
+            &[a, _, _, b, ..] => (a.parse::<u8>().unwrap(), b.parse::<u8>().unwrap()),
+            _ => panic!(),
+        };
+
+        Self {
+            id,
+            ore,
+            clay,
+            obsidian,
+            geode,
+        }
+    }
+}
+
+impl Blueprint {
+    fn run(&self, minutes: u16) -> u16 {
+        minutes * self.id as u16
+    }
+}
+
 struct Day19;
 impl Solution for Day19 {
     fn solve_part_one(input: String) -> String {
+        let blueprints = input.lines().map(Blueprint::from);
+
+        // blueprints
+        //     .map(|blueprint| blueprint.run(24))
+        //     .sum::<u16>()
+        //     .to_string()
+
         String::new()
     }
 
-    fn solve_part_two(input: String) -> String {
+    fn solve_part_two(_input: String) -> String {
         String::new()
     }
 }
--- AdventOfCode.Solutions/Year2022/Day19/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day19/Solution.cs
@@ -2,15 +2,9 @@ namespace AdventOfCode.Solutions.Year2022.Day19;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(19, 2022, "") { }
+    public Solution() : base(19, 2022, "Not Enough Minerals") { }
 
-    protected override string SolvePartOne()
-    {
-        return "";
-    }
+    protected override string SolvePartOne() => RustSolver.Solve(Year, Day, 1, Input);
 
-    protected override string SolvePartTwo()
-    {
-        return "";
-    }
+    protected override string SolvePartTwo() => RustSolver.Solve(Year, Day, 2, Input);
 }
