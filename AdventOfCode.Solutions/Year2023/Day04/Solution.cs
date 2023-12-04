5d1d024154c260d151a1d63a2135611a010a10b1
Sindre Kjær <code@sindrekjr.dev>
2023-12-04 06:24:13 +0100

    Solved Day 4 Part 1

--- AdventOfCode.Solutions/Year2023/Day04/Solution.cs
+++ AdventOfCode.Solutions/Year2023/Day04/Solution.cs
@@ -1,12 +1,37 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode.Solutions.Year2023.Day04;
 
-class Solution : SolutionBase
+partial class Solution : SolutionBase
 {
-    public Solution() : base(04, 2023, "") { }
+    public Solution() : base(04, 2023, "Scratchcards") { }
+
+    [GeneratedRegex(@"\s+")]
+    private static partial Regex SpaceRegex();
 
     protected override string SolvePartOne()
     {
-        return "";
+        return Input.SplitByNewline().Select(card =>
+        {
+            var (_, points, _) = card.Split(":");
+            var (winners, owned, _) = points
+                .Split("|", StringSplitOptions.TrimEntries)
+                .Select(str => SpaceRegex().Split(str).Select(int.Parse))
+                .ToArray();
+
+            var sum = 0;
+            foreach (var n in owned)
+            {
+                if (winners.Contains(n))
+                {
+                    sum = sum == 0
+                        ? 1
+                        : sum * 2;
+                }
+            }
+
+            return sum;
+        }).Sum().ToString();
     }
 
     protected override string SolvePartTwo()
