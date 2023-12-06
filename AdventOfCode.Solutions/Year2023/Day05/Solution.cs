c86051e13abe6dbc65b7dfa7a24bc4c598a45f21
Sindre Kjær <code@sindrekjr.dev>
2023-12-06 20:43:53 +0100

    Cleanup (??) Day 5

--- AdventOfCode.Solutions/Year2023/Day05/Solution.cs
+++ AdventOfCode.Solutions/Year2023/Day05/Solution.cs
@@ -43,23 +43,102 @@ class Solution : SolutionBase
 
     protected override string SolvePartTwo()
     {
-        return "";
-    }
+        var (seeds, paragraphs) = Input.SplitByParagraph();
 
-    long GetLocation(Dictionary<(long, long), (long, long)>[] maps, long seed)
-    {
-        return maps.Aggregate(seed, (source, map) =>
+        var seedsArray = seeds.Split(": ").Last().ToLongArray(" ");
+        var seedRanges = new List<(long start, long end)>();
+        for (var i = 0; i < seedsArray.Length; i++)
         {
-            foreach (var ((sStart, sEnd), (dStart, dEnd)) in map)
+            var start = seedsArray[i++];
+            var end = start + seedsArray[i];
+            seedRanges.Add((start, end));
+        }
+
+        var maps = paragraphs.Select(data =>
+        {
+            var map = new Dictionary<(long start, long end), (long start, long end)>();
+            foreach (var line in data.SplitByNewline().Skip(1))
             {
-                if (source >= sStart && source <= sEnd)
+                var values = line.ToLongArray(" ");
+                var length = values[2];
+
+                var destination =
+                (
+                    values[0],
+                    values[0] + length - 1
+                );
+                var source =
+                (
+                    values[1],
+                    values[1] + length - 1
+                );
+                map.Add(source, destination);
+            }
+
+            return map;
+        }).ToArray();
+
+        foreach (var map in maps)
+        {
+            var seedsExpanded = new List<(long, long)>();
+
+            for (var i = 0; i < seedRanges.Count; i++)
+            // foreach (var (start, end) in seedRanges)
+            {
+                var (start, end) = seedRanges[i];
+
+                var match = map.FirstOrDefault(kv =>
+                    kv.Key.start <= end && kv.Key.end >= start);
+
+                if (match.Key.start == 0 && match.Key.end == 0)
                 {
-                    var destination = source + dStart - sStart;
-                    return destination;
+                    seedsExpanded.Add((start, end));
+                    continue;
+                }
+
+                var (target, source, length) = (match.Value.start, match.Key.start, match.Key.end - match.Key.start + 1);
+
+                if (source <= start && source + length - 1 >= end)
+                {
+                    seedsExpanded.Add((start - source + target, end - source + target));
+                }
+                else if (source > start && source + length - 1 >= end)
+                {
+                    seedsExpanded.Add((target, end - source + target));
+                    seedRanges.Add((start, source - 1));
+                }
+                else if (source <= start && source + length - 1 < end)
+                {
+                    seedsExpanded.Add((start - source + target, target + length - 1));
+                    seedRanges.Add((source + length, end));
+                }
+                else if (source > start && source + length - 1 < end)
+                {
+                    seedsExpanded.Add((target, target + length - 1));
+                    seedRanges.Add((start, source - 1));
+                    seedRanges.Add((source + length, end));
                 }
             }
 
-            return source;
-        });
+            seedRanges = seedsExpanded;
+        }
+
+        return seedRanges.Min(range => range.start).ToString();
+    }
+
+    long GetLocation(Dictionary<(long, long), (long, long)>[] maps, long seed) =>
+        maps.Aggregate(seed, (seed, map) => GetLocation(map, seed));
+
+    long GetLocation(Dictionary<(long start, long end), (long start, long end)> map, long seed)
+    {
+        foreach (var (source, destination) in map)
+        {
+            if (seed >= source.start && seed <= source.end)
+            {
+                return seed + destination.start - source.start;
+            }
+        }
+
+        return seed;
     }
 }
