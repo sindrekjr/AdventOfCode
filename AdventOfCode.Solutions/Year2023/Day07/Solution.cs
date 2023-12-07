5e7dbceeb6b1be6bafcfd158206e9c2ed2bd3e3b
Sindre Kjær <code@sindrekjr.dev>
2023-12-07 16:07:58 +0100

    Solved Day 7 Part 2

--- AdventOfCode.Solutions/Year2023/Day07/Solution.cs
+++ AdventOfCode.Solutions/Year2023/Day07/Solution.cs
@@ -19,7 +19,15 @@ class Solution : SolutionBase
 
     protected override string SolvePartTwo()
     {
-        return "";
+        var hands = Input.SplitByNewline().Select(line => new CamelCardHand(line, true)).Order().ToArray();
+
+        var winnings = 0;
+        for (var i = 0; i < hands.Length; i++)
+        {
+            winnings += (i + 1) * hands[i].Bid;
+        }
+
+        return winnings.ToString();
     }
 }
 
@@ -40,7 +48,7 @@ internal class CamelCardHand : IComparable<CamelCardHand>
     internal int[] Cards { get; }
     internal int Bid { get; }
 
-    internal CamelCardHand(string input)
+    internal CamelCardHand(string input, bool joker = false)
     {
         var (cards, bid, _) = input.Split(" ");
 
@@ -50,7 +58,7 @@ internal class CamelCardHand : IComparable<CamelCardHand>
             : card switch
             {
                 'T' => 10,
-                'J' => 11,
+                'J' => joker ? 1 : 11,
                 'Q' => 12,
                 'K' => 13,
                 'A' => 14,
@@ -62,23 +70,52 @@ internal class CamelCardHand : IComparable<CamelCardHand>
 
     internal HandType ParseHandType()
     {
-        var (first, rest) = Cards
+        var counted = Cards
             .Distinct()
+            .Where(card => card != 1)
             .Select(card => Cards.Count(c => c == card))
             .OrderByDescending(count => count)
             .ToArray();
 
-        return first switch
+        if (counted.Length is 0 or 1) return HandType.FiveOfAKind;
+
+        var first = counted[0];
+        var second = counted.Length > 1 ? counted[1] : default;
+
+        var type = first switch
         {
             5 => HandType.FiveOfAKind,
             4 => HandType.FourOfAKind,
-            3 when rest.FirstOrDefault() == 2 => HandType.FullHouse,
+            3 when second == 2 => HandType.FullHouse,
             3 => HandType.ThreeOfAKind,
-            2 when rest.FirstOrDefault() == 2 => HandType.TwoPair,
+            2 when second == 2 => HandType.TwoPair,
             2 => HandType.OnePair,
             1 => HandType.HighCard,
+
             _ => throw new InvalidOperationException()
         };
+
+        var jokers = Cards.Count(card => card == 1);
+        return jokers == 0
+            ? type
+            : type switch
+            {
+                HandType.FiveOfAKind when jokers is 5 => HandType.FiveOfAKind,
+                HandType.FourOfAKind => HandType.FiveOfAKind,
+                HandType.ThreeOfAKind when jokers is 1 => HandType.FourOfAKind,
+                HandType.ThreeOfAKind when jokers is 2 => HandType.FiveOfAKind,
+                HandType.TwoPair => HandType.FullHouse,
+                HandType.OnePair when second is 2 => HandType.FullHouse,
+                HandType.OnePair when jokers is 1 => HandType.ThreeOfAKind,
+                HandType.OnePair when jokers is 2 => HandType.FourOfAKind,
+                HandType.OnePair when jokers is 3 => HandType.FiveOfAKind,
+                HandType.HighCard when jokers is 1 => HandType.OnePair,
+                HandType.HighCard when jokers is 2 => HandType.ThreeOfAKind,
+                HandType.HighCard when jokers is 3 => HandType.FourOfAKind,
+                HandType.HighCard when jokers is 4 => HandType.FiveOfAKind,
+
+                _ => throw new InvalidOperationException()
+            };
     }
 
     public int CompareTo(CamelCardHand? other)
