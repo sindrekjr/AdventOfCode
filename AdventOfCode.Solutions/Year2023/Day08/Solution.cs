3ab5514a6a98137e6644da75f175bdfb790375ae
Sindre Kjær <code@sindrekjr.dev>
2023-12-08 08:04:02 +0100

    Solved Day 8 Part 1

--- AdventOfCode.Solutions/Year2023/Day08/Solution.cs
+++ AdventOfCode.Solutions/Year2023/Day08/Solution.cs
@@ -2,11 +2,39 @@ namespace AdventOfCode.Solutions.Year2023.Day08;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(08, 2023, "") { }
+    public Solution() : base(08, 2023, "Haunted Wasteland") { }
 
     protected override string SolvePartOne()
     {
-        return "";
+        var lines = Input.SplitByNewline();
+        var instructions = lines.First().ToCharArray();
+
+        var mappings = new Dictionary<string, (string, string)>();
+        foreach (var line in lines.Skip(1))
+        {
+            var (node, leftright, _) = line.Split(" = ");
+            var (left, right, _) = leftright.Trim(['(', ')']).Split(",", StringSplitOptions.TrimEntries);
+            mappings.Add(node, (left, right));
+        }
+
+        var steps = 0;
+        var instruction = 0;
+        var current = "AAA";
+        while (current != "ZZZ")
+        {
+            if (instruction == instructions.Length)
+            {
+                instruction = 0;
+            }
+
+            steps++;
+            var (left, right) = mappings[current];
+            current = instructions[instruction++] is 'L'
+                ? left
+                : right;
+        }
+
+        return steps.ToString();
     }
 
     protected override string SolvePartTwo()
