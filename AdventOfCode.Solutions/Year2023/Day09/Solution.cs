4b590b775254f548aca913cfcc7c513c37c37146
Sindre Kjær <code@sindrekjr.dev>
2023-12-09 09:38:14 +0100

    Solved Day 9

--- AdventOfCode.Solutions/Year2023/Day09/Solution.cs
+++ AdventOfCode.Solutions/Year2023/Day09/Solution.cs
@@ -1,16 +1,39 @@
+
 namespace AdventOfCode.Solutions.Year2023.Day09;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(09, 2023, "") { }
+    public Solution() : base(09, 2023, "Mirage Maintenance") { }
 
     protected override string SolvePartOne()
     {
-        return "";
+        return Input.SplitByNewline().Aggregate(0, (sum, line) =>
+        {
+            return sum + PredictNextValue(line.ToIntArray(" "));
+        }).ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        return Input.SplitByNewline().Aggregate(0, (sum, line) =>
+        {
+            return sum + PredictNextValue(line.ToIntArray(" ").Reverse().ToArray());
+        }).ToString();
+    }
+
+    private int PredictNextValue(int[] values)
+    {
+        var diff = new int[values.Length - 1];
+        for (var i = 1; i < values.Length; i++)
+        {
+            diff[i - 1] = values[i] - values[i - 1];
+        }
+
+        if (diff.All(n => n is 0))
+        {
+            return values.Last();
+        }
+
+        return values.Last() + PredictNextValue(diff);
     }
 }
