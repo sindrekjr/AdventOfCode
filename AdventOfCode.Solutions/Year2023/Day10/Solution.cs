dceaafe0b1c2a16b8f77753f25c664ac1080f197
Sindre Kjær <code@sindrekjr.dev>
2023-12-10 20:18:28 +0100

    Solved Day 10 Part 1

--- AdventOfCode.Solutions/Year2023/Day10/Solution.cs
+++ AdventOfCode.Solutions/Year2023/Day10/Solution.cs
@@ -1,16 +1,45 @@
+using AdventOfCode.Solutions.Year2020;
+
 namespace AdventOfCode.Solutions.Year2023.Day10;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(10, 2023, "") { }
+    public Solution() : base(10, 2023, "Pipe Maze") { }
 
     protected override string SolvePartOne()
     {
-        return "";
+        var map = new SquareMap<char>(Input.SplitByNewline().Select(line => line.ToCharArray()).ToArray());
+        var start = map.First(kv => kv.Value == 'S');
+        return (FindLoop(map, [start.Key], start.Key).Length / 2).ToString();
     }
 
     protected override string SolvePartTwo()
     {
         return "";
+    }
+
+    (int, int)[] FindLoop(SquareMap<char> map, IEnumerable<(int, int)> loop, (int, int) last)
+    {
+        var (x, y) = last;
+        (int, int)[] candidates = map[last] switch
+        {
+            '|' => [(x + 1, y), (x - 1, y)],
+            '-' => [(x, y + 1), (x, y - 1)],
+            'L' => [(x - 1, y), (x, y + 1)],
+            'J' => [(x - 1, y), (x, y - 1)],
+            '7' => [(x + 1, y), (x, y - 1)],
+            'F' => [(x + 1, y), (x, y + 1)],
+            'S' => [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)],
+
+            _ => throw new InvalidOperationException(),
+        };
+
+        if (loop.Count() > 2 && candidates.Any(c => map.GetValueOrDefault(c) == 'S'))
+        {
+            return loop.ToArray();
+        }
+
+        var next = candidates.Where(c => !loop.Contains(c)).First();
+        return FindLoop(map, loop.Append(next), next);
     }
 }
