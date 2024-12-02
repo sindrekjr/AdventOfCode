c8c5b464a322e053daceab0c13276e15399659d2
Sindre Kjær <code@sindrekjr.dev>
2024-12-02 07:06:51 +0100

    Solved Day 2

--- AdventOfCode.Solutions.Rust/src/y2024/d02.rs
+++ AdventOfCode.Solutions.Rust/src/y2024/d02.rs
@@ -10,10 +10,60 @@ pub fn solve(part: Part, input: String) -> String {
 struct Day02;
 impl Solution for Day02 {
     fn solve_part_one(input: String) -> String {
-        format!("Input is {}", input)
+        input
+            .lines()
+            .filter(|l| {
+                let levels: Vec<i32> = l
+                    .split_whitespace()
+                    .map(|level| level.parse::<i32>().unwrap())
+                    .collect();
+
+                safe(levels)
+            })
+            .count()
+            .to_string()
     }
 
     fn solve_part_two(input: String) -> String {
-        format!("Input is {}", input)
+        input
+            .lines()
+            .filter(|l| {
+                let levels: Vec<i32> = l
+                    .split_whitespace()
+                    .map(|level| level.parse::<i32>().unwrap())
+                    .collect();
+
+                (0..levels.len()).any(|i| {
+                    let new_levels: Vec<i32> = levels[..i]
+                        .iter()
+                        .chain(&levels[i + 1..])
+                        .cloned()
+                        .collect();
+                    safe(new_levels)
+                })
+            })
+            .count()
+            .to_string()
     }
+}
+
+fn safe(levels: Vec<i32>) -> bool {
+    let increasing = levels[0] < levels[1];
+    let mut previous: Option<i32> = None;
+
+    for level in levels {
+        if let Some(p) = previous {
+            if p == level || (p - level).abs() > 3 {
+                return false;
+            }
+
+            if increasing && level < p || !increasing && level > p {
+                return false;
+            }
+        }
+
+        previous = Some(level);
+    }
+
+    true
 }
--- AdventOfCode.Solutions/Year2024/Day02/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day02/Solution.cs
@@ -2,7 +2,7 @@ namespace AdventOfCode.Solutions.Year2024.Day02;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(02, 2024, "") { }
+    public Solution() : base(02, 2024, "Red-Nosed Reports") { }
 
     protected override string SolvePartOne() => RustSolver.Solve(Year, Day, 1, Input);
 
