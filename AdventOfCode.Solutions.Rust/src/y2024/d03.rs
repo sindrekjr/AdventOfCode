2f5bc2e5d8982a08bf790ac8eef780da579d7534
Sindre Kjær <code@sindrekjr.dev>
2024-12-03 06:48:08 +0100

    Solved Day 3

--- AdventOfCode.Solutions.Rust/src/y2024/d03.rs
+++ AdventOfCode.Solutions.Rust/src/y2024/d03.rs
@@ -10,10 +10,35 @@ pub fn solve(part: Part, input: String) -> String {
 struct Day03;
 impl Solution for Day03 {
     fn solve_part_one(input: String) -> String {
-        format!("Input is {}", input)
+        regex::Regex::new(r"mul\((\d+),(\d+)\)")
+            .unwrap()
+            .captures_iter(&input)
+            .fold(0, |sum, cap| {
+                sum + cap[1].parse::<u32>().unwrap() * cap[2].parse::<u32>().unwrap()
+            })
+            .to_string()
     }
 
     fn solve_part_two(input: String) -> String {
-        format!("Input is {}", input)
+        regex::Regex::new(r"do\(\)|mul\((\d+),(\d+)\)|don't\(\)")
+            .unwrap()
+            .captures_iter(&input)
+            .fold((0, true), |(sum, enabled), cap| {
+                if let (Some(a), Some(b)) = (cap.get(1), cap.get(2)) {
+                    if enabled {
+                        (
+                            sum + a.as_str().parse::<u32>().unwrap()
+                                * b.as_str().parse::<u32>().unwrap(),
+                            enabled,
+                        )
+                    } else {
+                        (sum, enabled)
+                    }
+                } else {
+                    (sum, cap[0].to_string() == "do()")
+                }
+            })
+            .0
+            .to_string()
     }
 }
--- AdventOfCode.Solutions/Year2024/Day03/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day03/Solution.cs
@@ -2,7 +2,7 @@ namespace AdventOfCode.Solutions.Year2024.Day03;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(03, 2024, "") { }
+    public Solution() : base(03, 2024, "Mull It Over") { }
 
     protected override string SolvePartOne() => RustSolver.Solve(Year, Day, 1, Input);
 
