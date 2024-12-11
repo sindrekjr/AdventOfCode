76f065ba172fac0daab18e8deebeb9cd0f11d41c
Sindre Kjær <code@sindrekjr.dev>
2024-12-11 16:05:55 +0100

    Solved Day 11

--- AdventOfCode.Solutions.Rust/src/y2024/d11.rs
+++ AdventOfCode.Solutions.Rust/src/y2024/d11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::core::{Part, Solution};
 
 pub fn solve(part: Part, input: String) -> String {
@@ -10,10 +12,50 @@ pub fn solve(part: Part, input: String) -> String {
 struct Day11;
 impl Solution for Day11 {
     fn solve_part_one(input: String) -> String {
-        format!("Input is {}", input)
+        let blinks = 25;
+        let mut memo: HashMap<(u64, u8), u64> = HashMap::new();
+        input
+            .split_whitespace()
+            .map(|str| stonecount(str.parse().unwrap(), blinks, &mut memo))
+            .sum::<u64>()
+            .to_string()
     }
 
     fn solve_part_two(input: String) -> String {
-        format!("Input is {}", input)
+        let blinks = 75;
+        let mut memo: HashMap<(u64, u8), u64> = HashMap::new();
+        input
+            .split_whitespace()
+            .map(|str| stonecount(str.parse().unwrap(), blinks, &mut memo))
+            .sum::<u64>()
+            .to_string()
     }
+}
+
+fn stonecount(stone: u64, blinks: u8, memo: &mut HashMap<(u64, u8), u64>) -> u64 {
+    if blinks == 0 {
+        return 1;
+    }
+
+    if let Some(count) = memo.get(&(stone, blinks)) {
+        return *count;
+    }
+
+    let count = match stone {
+        0 => stonecount(1, blinks - 1, memo),
+        num => {
+            let num_str = num.to_string();
+            if num_str.len() % 2 == 0 {
+                let (left, right) = num_str.split_at(num_str.len() / 2);
+
+                stonecount(left.parse().unwrap_or(0), blinks - 1, memo)
+                    + stonecount(right.parse().unwrap_or(0), blinks - 1, memo)
+            } else {
+                stonecount(num * 2024, blinks - 1, memo)
+            }
+        }
+    };
+
+    memo.insert((stone, blinks), count);
+    count
 }
--- AdventOfCode.Solutions/Year2024/Day11/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day11/Solution.cs
@@ -2,7 +2,7 @@ namespace AdventOfCode.Solutions.Year2024.Day11;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(11, 2024, "") { }
+    public Solution() : base(11, 2024, "Plutonian Pebbles") { }
 
     protected override string SolvePartOne() => RustSolver.Solve(Year, Day, 1, Input);
 
