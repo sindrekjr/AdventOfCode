0b4d8af588747503e7b211d4789d52a8fc48966b
Sindre Kjær <code@sindrekjr.dev>
2024-12-12 07:16:14 +0100

    Solved Day 12 Part 1

--- AdventOfCode.Solutions.Rust/src/y2024/d12.rs
+++ AdventOfCode.Solutions.Rust/src/y2024/d12.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use crate::core::{Part, Solution};
 
 pub fn solve(part: Part, input: String) -> String {
@@ -7,13 +9,71 @@ pub fn solve(part: Part, input: String) -> String {
     }
 }
 
+const DIRECTIONS: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
 struct Day12;
 impl Solution for Day12 {
     fn solve_part_one(input: String) -> String {
-        format!("Input is {}", input)
+        let map = parse_map(&input);
+        let mut visited: HashSet<(isize, isize)> = HashSet::new();
+
+        map.iter()
+            .map(|((x, y), plant)| {
+                let (region, perimeter) = find_region((*x, *y, *plant), &map, &mut visited);
+                region.len() * perimeter.len()
+            })
+            .sum::<usize>()
+            .to_string()
     }
 
     fn solve_part_two(input: String) -> String {
-        format!("Input is {}", input)
+        String::new()
     }
+}
+
+fn parse_map(input: &String) -> HashMap<(isize, isize), char> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.char_indices()
+                .map(|(x, ch)| ((x as isize, y as isize), ch))
+                .collect::<Vec<_>>()
+        })
+        .collect()
+}
+
+fn find_region(
+    position: (isize, isize, char),
+    map: &HashMap<(isize, isize), char>,
+    visited: &mut HashSet<(isize, isize)>,
+) -> (HashSet<(isize, isize)>, Vec<(isize, isize)>) {
+    let (x, y, plant) = position;
+    let mut region = HashSet::new();
+    let mut perimeter = vec![];
+    region.insert((x, y));
+
+    if visited.contains(&(x, y)) {
+        return (region, perimeter);
+    } else {
+        visited.insert((x, y));
+    }
+
+    DIRECTIONS.iter().for_each(|(x_d, y_d)| {
+        let adjacent = ((x + x_d), (y + y_d));
+
+        match map.get(&adjacent) {
+            Some(adjacent_plant) if &plant == adjacent_plant => {
+                let (sub_reg, sub_per) =
+                    find_region((adjacent.0, adjacent.1, *adjacent_plant), map, visited);
+                region.extend(sub_reg);
+                perimeter.extend(sub_per);
+            }
+            _ => {
+                perimeter.push(adjacent);
+            }
+        }
+    });
+
+    return (region, perimeter);
 }
--- AdventOfCode.Solutions/Year2024/Day12/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day12/Solution.cs
@@ -2,7 +2,7 @@ namespace AdventOfCode.Solutions.Year2024.Day12;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(12, 2024, "") { }
+    public Solution() : base(12, 2024, "Garden Groups") { }
 
     protected override string SolvePartOne() => RustSolver.Solve(Year, Day, 1, Input);
 
