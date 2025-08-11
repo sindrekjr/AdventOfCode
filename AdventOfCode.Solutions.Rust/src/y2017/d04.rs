ef97748894a75c58eb9bb7956f41f822c2781c8c
Sindre Kjær <code@sindrekjr.dev>
2025-08-11 20:52:53 +0200

    Solved Day 4, 2017

--- AdventOfCode.Solutions.Rust/src/y2017/d04.rs
+++ AdventOfCode.Solutions.Rust/src/y2017/d04.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::core::{Part, Solution};
 
 pub fn solve(part: Part, input: String) -> String {
@@ -12,10 +14,33 @@ struct Day04;
 #[allow(unused_variables)]
 impl Solution for Day04 {
     fn solve_part_one(input: String) -> String {
-        String::from("Unsolved")
+        input
+            .lines()
+            .filter(|line| {
+                let split = line.split_whitespace();
+                split.clone().collect::<HashSet<&str>>().len() == split.count()
+            })
+            .count()
+            .to_string()
     }
 
     fn solve_part_two(input: String) -> String {
-        String::from("Unsolved")
+        input
+            .lines()
+            .filter(|line| {
+                let split = line.split_whitespace();
+                split
+                    .clone()
+                    .map(|str| {
+                        let mut chars = str.chars().collect::<Vec<char>>();
+                        chars.sort();
+                        chars
+                    })
+                    .collect::<HashSet<Vec<char>>>()
+                    .len()
+                    == split.count()
+            })
+            .count()
+            .to_string()
     }
 }
