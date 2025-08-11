e9c372e6668f0674cc9ad390636018eb46b822a0
Sindre Kjær <code@sindrekjr.dev>
2025-08-11 20:41:21 +0200

    Solved Day 2, 2017

--- AdventOfCode.Solutions.Rust/src/y2017/d02.rs
+++ AdventOfCode.Solutions.Rust/src/y2017/d02.rs
@@ -12,10 +12,35 @@ struct Day02;
 #[allow(unused_variables)]
 impl Solution for Day02 {
     fn solve_part_one(input: String) -> String {
-        String::from("Unsolved")
+        input
+            .lines()
+            .map(|line| {
+                let (min, max) = line
+                    .split_whitespace()
+                    .fold((i32::MAX, i32::MIN), |(min, max), n| {
+                        (min.min(n.parse().unwrap()), max.max(n.parse().unwrap()))
+                    });
+
+                max - min
+            })
+            .sum::<i32>()
+            .to_string()
     }
 
     fn solve_part_two(input: String) -> String {
-        String::from("Unsolved")
+        input
+            .lines()
+            .map(|line| {
+                let nums = line
+                    .split_whitespace()
+                    .map(|n| n.parse().unwrap())
+                    .collect::<Vec<i32>>();
+
+                nums.iter()
+                    .find_map(|n| nums.iter().find(|m| n != *m && n % *m == 0).map(|m| n / m))
+                    .unwrap()
+            })
+            .sum::<i32>()
+            .to_string()
     }
 }
