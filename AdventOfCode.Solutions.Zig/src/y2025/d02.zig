c6fe5fd4d3bbf461c0797404c233abf072a79d86
Sindre Kjær <code@sindrekjr.dev>
2025-12-06 23:59:26 +0100

    Solve Day 2 Part 2

--- AdventOfCode.Solutions.Zig/src/y2025/d02.zig
+++ AdventOfCode.Solutions.Zig/src/y2025/d02.zig
@@ -47,6 +47,58 @@ pub fn solvePartOne(input: []const u8) ?[*]u8 {
 }
 
 pub fn solvePartTwo(input: []const u8) ?[*]u8 {
-    _ = input;
-    return null;
+    const allocator = std.heap.page_allocator;
+
+    var ranges: std.ArrayList([2]usize) = .empty;
+    defer ranges.deinit(allocator);
+
+    var min_len: usize = std.math.maxInt(usize);
+    var max_len: usize = 0;
+
+    var lines = std.mem.tokenizeScalar(u8, std.mem.trim(u8, input, "\n"), ',');
+    while (lines.next()) |line| {
+        if (line.len == 0) continue;
+        var split = std.mem.splitScalar(u8, line, '-');
+
+        const start_str = split.next().?;
+        const start_len = start_str.len;
+        const start = std.fmt.parseUnsigned(usize, start_str, 10) catch unreachable;
+        if (start_len < min_len) min_len = start_len;
+
+        const end_str = split.next().?;
+        const end_len = end_str.len;
+        const end = std.fmt.parseUnsigned(usize, end_str, 10) catch unreachable;
+        if (end_len > max_len) max_len = end_len;
+
+        ranges.append(allocator, [_]usize{ start, end }) catch unreachable;
+    }
+
+    var sum: usize = 0;
+    var checked: std.AutoHashMap(usize, bool) = std.AutoHashMap(usize, bool).init(allocator);
+    defer checked.deinit();
+    for (min_len..max_len + 1) |length| {
+        for (1..(length / 2) + 1) |part_length| {
+            if (length % part_length != 0) continue;
+            const repetitions = length / part_length;
+
+            const start = std.math.pow(usize, 10, part_length - 1);
+            const end = std.math.pow(usize, 10, part_length);
+
+            for (start..end) |partition| {
+                var n = partition;
+                for (0..repetitions - 1) |_| n = (n * std.math.pow(usize, 10, part_length)) + partition;
+                if (checked.contains(n)) continue;
+
+                checked.put(n, true) catch unreachable;
+                for (ranges.items) |range| {
+                    if (range[0] <= n and n <= range[1]) {
+                        sum += n;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    return core.toString(usize, &allocator, sum);
 }
