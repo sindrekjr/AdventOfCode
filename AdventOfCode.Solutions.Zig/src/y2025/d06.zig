3e67b35bec8b61dcaea1c43a5a2f0a5978ff0113
Sindre Kjær <code@sindrekjr.dev>
2025-12-06 12:59:54 +0100

    Solve Day 6 Part 1

--- AdventOfCode.Solutions.Zig/src/y2025/d06.zig
+++ AdventOfCode.Solutions.Zig/src/y2025/d06.zig
@@ -10,8 +10,47 @@ pub fn solve(part: core.Part, input: []const u8) ?[*]u8 {
 }
 
 pub fn solvePartOne(input: []const u8) ?[*]u8 {
-    _ = input;
-    return null;
+    const allocator = std.heap.page_allocator;
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+
+    var numbers: std.ArrayList([]const u64) = .empty;
+    defer numbers.deinit(allocator);
+
+    var i: usize = 0;
+    var result: u64 = 0;
+    while (lines.next()) |line| {
+        var items = std.mem.tokenizeScalar(u8, line, ' ');
+
+        var row: std.ArrayList(u64) = .empty;
+        while (items.next()) |item| {
+            if (item.len == 0) continue;
+            switch (item[0]) {
+                '*' => {
+                    var product: u64 = 1;
+                    for (numbers.items) |prev_row| {
+                        product *= prev_row[i];
+                    }
+                    result += product;
+                    i += 1;
+                },
+                '+' => {
+                    var sum: u64 = 0;
+                    for (numbers.items) |prev_row| {
+                        sum += prev_row[i];
+                    }
+                    result += sum;
+                    i += 1;
+                },
+                else => {
+                    const n = std.fmt.parseUnsigned(u16, item, 10) catch unreachable;
+                    row.append(allocator, n) catch unreachable;
+                },
+            }
+        }
+        numbers.append(allocator, row.items[0..]) catch unreachable;
+    }
+
+    return core.toString(u64, &allocator, result);
 }
 
 pub fn solvePartTwo(input: []const u8) ?[*]u8 {
